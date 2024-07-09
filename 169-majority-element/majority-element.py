9adf7f62094099779ee9ca9695fa18229bd7ad38
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-10 00:28:01 +0300

    Time: 154 ms (81.88%) | Memory: 13.2 MB (57.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,18 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count_map = {}
-
-        for i in range(len(nums)):
-            if nums[i] in count_map:
-                count_map[nums[i]] += 1
-            else:
-                count_map[nums[i]] = 1
-        
-        max_count = 0
-        for i in range(len(nums)):
-            if count_map[nums[i]] > max_count:
-                max_count = count_map[nums[i]]
-                majority_num = nums[i]
-        
-        return majority_num
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]
