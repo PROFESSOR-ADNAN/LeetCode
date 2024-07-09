d1cfa7dcf2bcab3cc9d1db77e0ade9c023afd8fe
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-10 00:30:22 +0300

    Time: 160 ms (58.62%) | Memory: 13.2 MB (57.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        n = len(nums)
-        return nums[n//2]
+        count = 0
+        candidate = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
