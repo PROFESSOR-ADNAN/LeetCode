5974b680c5c63a917ff1a85cd65ae695e21e2e9e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-22 16:52:08 +0300

    Time: 117 ms (61.46%) | Memory: 13.1 MB (20.75%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -7,15 +7,11 @@ class Solution(object):
         """
         g.sort()
         s.sort()
-        counter = 0
         i = 0
         j = 0
         while j < len(s):
             if i < len(g) and s[j] >= g[i]:
-                counter += 1
                 i += 1
-                j += 1
-            else:
-                j += 1
-        return counter
-            
+            j += 1
+        return i
+                
