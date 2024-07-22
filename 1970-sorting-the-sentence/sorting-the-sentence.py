4f2bdc6f40adf96fc8ad2a7b183259a351af8bcb
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-22 08:07:08 +0300

    Time: 13 ms (55.29%) | Memory: 11.8 MB (19.87%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -3,7 +3,15 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """   
-        arr = s.split(" ")
-        arr.sort(key = lambda x: int(x[-1]))
-        return " ".join(x[:-1] for x in arr)
+        """
+        hash_map = {}
+        splited_list = s.split(" ")
+        ans = ""
+
+        for word in splited_list:
+            hash_map[int(word[-1])] = word[:-1:]
+        for i in range(1, len(splited_list) + 1):
+            ans += hash_map[i]
+            if i != len(splited_list):
+                ans += " "
+        return ans
