3abb100179edfa48cbd1cb8658d37a56bc16a68c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-29 12:21:18 +0300

    Time: 18 ms (64.52%) | Memory: 11.7 MB (31.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -10,15 +10,15 @@ class Solution(object):
         l = m - 1
         r = n - 1
         k = len(nums1) - 1
-        while l >= 0 and r >= 0:
-            if nums1[l] > nums2[r]:
-                nums1[k] = nums1[l]
-                l -= 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[k] = nums1[m - 1]
+                m -= 1
             else:
-                nums1[k] = nums2[r]
-                r -= 1
+                nums1[k] = nums2[n - 1]
+                n -= 1
             k -= 1
-        while r >= 0:
-            nums1[k] = nums2[r]
-            k, r = k - 1, r - 1
+        while n > 0:
+            nums1[k] = nums2[n - 1]
+            k, n = k - 1, n - 1
         return nums1
