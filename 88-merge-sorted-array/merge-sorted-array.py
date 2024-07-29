4c325c43f07882c244a3790d7eb3db9233922870
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-29 10:58:11 +0300

    Time: 13 ms (90.44%) | Memory: 11.6 MB (68.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,22 +7,7 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        nums3 = []
-        left = 0
-        right = 0
-        while left < m and right < n:
-            if nums1[left] <= nums2[right]:
-                nums3.append(nums1[left])
-                left += 1
-            else:
-                nums3.append(nums2[right])
-                right += 1
-        while left < m:
-            nums3.append(nums1[left])
-            left += 1
-        while right < n:
-            nums3.append(nums2[right])
-            right += 1
-        for i in range(n+m):
-            nums1[i] = nums3[i]
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        nums1.sort()
         return nums1
