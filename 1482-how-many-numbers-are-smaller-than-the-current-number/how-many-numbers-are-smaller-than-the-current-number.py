f88aeb3e7d288a0b9cc8945d00ec92bab4b3c34c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-07-29 18:41:02 +0300

    Time: 32 ms (91.50%) | Memory: 11.6 MB (55.52%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = []
-        count = 0
-        for i in range(len(nums)):
-            current_element = nums[i]
-            for j in range(len(nums)):
-                if nums[j] < current_element:
-                    count += 1
-            result.append(count)
-            count = 0
-        return result
+        indexed_nums = [(nums[i], i) for i in range(len(nums))]
+        indexed_nums.sort()
+        print(indexed_nums)
+        for i in range(len(indexed_nums)):
+            if i > 0 and indexed_nums[i-1][0] == indexed_nums[i][0]:
+                nums[indexed_nums[i][1]] = nums[indexed_nums[i-1][1]]
+            else:
+                nums[indexed_nums[i][1]] = i
+        return nums
