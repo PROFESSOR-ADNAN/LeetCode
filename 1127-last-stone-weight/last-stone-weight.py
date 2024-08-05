cdb3f86909c5232db95a6e248433ec5bdaf3f73a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-05 23:47:53 +0300

    Time: 14 ms (70.18%) | Memory: 11.5 MB (80.18%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -8,13 +8,10 @@ class Solution(object):
         while len(stones) > 1:
             last = stones[len(stones) - 1]
             second_to_last = stones[len(stones) - 2]
-            if last == second_to_last:
-                stones.pop()
-                stones.pop()
-            else:
+            stones.pop()
+            stones.pop()
+            if last!= second_to_last:
                 diff = last - second_to_last
-                stones.pop()
-                stones.pop()
                 stones.append(diff)
                 stones.sort()
         return stones[0] if len(stones) > 0 else 0
