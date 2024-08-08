2d0317ddc00fa863853387642155b5742afb7963
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-08 03:47:56 +0300

    Time: 11 ms (88.05%) | Memory: 11.7 MB (47.45%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
-        stones = [x * -1 for x in stones]
-        print(stones)
+        stones = [-s for s in stones]
         heapq.heapify(stones)
         while len(stones) > 1:
             last = heapq.heappop(stones)
@@ -13,5 +12,6 @@ class Solution(object):
             if last != second_to_last:
                 diff = last - second_to_last
                 heapq.heappush(stones, diff)
-        return stones[0] * -1 if len(stones) > 0 else 0
+        stones.append(0)
+        return abs(stones[0])
         
