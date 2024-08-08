cdc665b86f659e39f0bc97a342e665320502901d
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-08 03:22:13 +0300

    Time: 11 ms (88.05%) | Memory: 11.8 MB (18.14%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
-        while len(stones) > 1:  
-            heapq._heapify_max(stones)
-            last = heapq.heappop(stones)
-            heapq._heapify_max(stones)
-            second_to_last = heapq.heappop(stones)
+        stones.sort()
+        while len(stones) > 1:
+            last = stones.pop()
+            second_to_last = stones.pop()
             if last != second_to_last:
                 diff = last - second_to_last
                 stones.append(diff)
-        return stones[0] if len(stones) > 0 else 0
+                stones.sort()
 
+        return stones[0] if len(stones) > 0 else 0
         
