e24c6f0bb369cdf8edea59ebb676c9dec0c57429
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-08 08:22:29 +0300

    Time: 841 ms (46.58%) | Memory: 22.2 MB (20.71%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -5,9 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # k = len(nums) - k
+        # def quickSelect(l, r):
+        #     pivot, p = nums[r], l
+        #     for i in range(l, r):
+        #         if nums[i] <= pivot:
+        #             nums[i], nums[p] = nums[p], nums[i]
+        #             p += 1
+        #     nums[p], nums[r] = pivot, nums[p]
+        #     if p > k: return quickSelect(l, p-1)
+        #     elif p < k: return quickSelect(p+1, r)
+        #     else: return nums[p]
+    
+        # return quickSelect(0, len(nums)-1)
         nums = [-num for num in nums]
         heapq.heapify(nums)
         for i in range(k):
-            kth_largest = heapq.heappop(nums)
-        return kth_largest * -1
-        
+            itm = heapq.heappop(nums)
+        return itm * -1        
