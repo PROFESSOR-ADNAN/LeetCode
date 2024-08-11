01655765fec5a42038bf0647dac1e77eb13af588
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-11 08:38:26 +0300

    Time: 1472 ms (43.87%) | Memory: 20.3 MB (52.52%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -4,12 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        heapq.heapify(nums)
-        ans = []
-        for i in range(len(nums)):
-            ans.append(heapq.heappop(nums)) 
-        return ans 
-        
+        def merge_sort(nums):
+            if len(nums) > 1:
+                mid = len(nums) // 2
+                left_list = nums[:mid]
+                right_list = nums[mid:]
+                merge_sort(left_list)
+                merge_sort(right_list)
+                l = r = k = 0
+                while l < len(left_list) and r < len(right_list):
+                    if left_list[l] < right_list[r]:
+                        nums[k] = left_list[l]
+                        k += 1
+                        l += 1
+                    else:
+                        nums[k] = right_list[r]
+                        k += 1
+                        r += 1
+                while l < len(left_list):
+                    nums[k] = left_list[l]
+                    k += 1
+                    l += 1
+                while r < len(right_list):
+                    nums[k] = right_list[r]
+                    k += 1
+                    r += 1
+            return nums
+        return merge_sort(nums)
+            
 
         
 
