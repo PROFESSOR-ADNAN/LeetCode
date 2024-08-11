05d3664486c6c027527d68654a27f229425ea08e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-11 12:13:24 +0300

    Time: 1338 ms (62.16%) | Memory: 21.4 MB (23.46%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -4,38 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        def merge(Arr, L, Mid, R):
-            left, right = Arr[L:Mid+1], Arr[Mid+1:R+1]
-            i, j, k = 0, 0, L
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    Arr[k] = left[i]
-                    i += 1
-                else:
-                    Arr[k] = right[j]
-                    j += 1
-                k += 1
-            while i < len(left):
-                Arr[k] = left[i]
-                i += 1
-                k += 1
-            while j < len(right):
-                Arr[k] = right[j]
-                j += 1
-                k += 1
-            
-
-        def mergeSort(arr, l, r):
-            if l == r:
-                return arr
-            mid = (l+r) // 2
-            mergeSort(arr, l, mid)
-            mergeSort(arr, mid+1, r)
-            merge(arr, l, mid, r)
-            return arr
-
-        return mergeSort(nums, 0, len(nums)-1)
-            
+        heapq.heapify(nums)
+        ans = []
+        for i in range(len(nums)):
+            ans.append(heapq.heappop(nums))
+        return ans
 
         
 
