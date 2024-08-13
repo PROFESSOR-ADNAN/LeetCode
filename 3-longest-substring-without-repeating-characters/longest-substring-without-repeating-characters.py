e43eeed43b6413a997fc86c9697cf6de4183a332
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-13 16:04:25 +0300

    Time: 3385 ms (5.00%) | Memory: 13.6 MB (6.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,15 +5,13 @@ class Solution(object):
         :rtype: int
         """
         max_len = 0
-        l = r = 0
-        seen = set()
-        while r < len(s):
-            while s[r] in seen:
-                seen.remove(s[l])
-                l += 1
-            if s[r] not in seen:
-                seen.add(s[r])
-                max_len = max(max_len, r-l+1)
-            r += 1
-
+        for i in range(len(s)):
+            seen = set()
+            for j in range(i, len(s)):
+                if s[j] not in seen:
+                    seen.add(s[j])
+                    max_len = max(max_len, j-i+1)
+                else:
+                    break
         return max_len
+
