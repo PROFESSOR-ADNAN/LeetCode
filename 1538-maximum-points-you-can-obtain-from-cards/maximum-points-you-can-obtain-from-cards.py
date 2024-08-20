563a095a9e4f73cdbab3c697a383c49c767996a1
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-20 20:28:32 +0300

    Time: 293 ms (96.81%) | Memory: 21.8 MB (75.65%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -5,18 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        window_sum = sum(cardPoints[:k])
+        max_sum = window_sum
         l = k-1
-        r = len(cardPoints) - 1
-        temp_sum = sum(cardPoints[:k])
-        max_sum = temp_sum
-        while k > 0:
-            temp_sum -= cardPoints[l]
+        r = len(cardPoints)-1
+        while l >= 0:
+            window_sum -= cardPoints[l]
             l -= 1
-            temp_sum += cardPoints[r]
+            window_sum += cardPoints[r]
             r -= 1
-            print("temp: ", temp_sum)
-            max_sum = max(max_sum, temp_sum)
-            print(max_sum)
-            k -= 1
+
+            max_sum = max(max_sum, window_sum)
+
         return max_sum
+
         
