8ebec6c899d9757b244c9a81662385ef61b35591
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-21 17:17:20 +0300

    Time: 273 ms (10.09%) | Memory: 12.1 MB (48.75%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -3,17 +3,17 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        """
+        """ 
         max_len = 0
-        l, r = 0, 0
-        while r < len(nums):
-            if nums[r] == 1:
-                max_len = max(max_len, r-l+1)
-            else:
-                l = r + 1
-            r += 1
+        l = 0
 
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                l = r+1
+            max_len = max(max_len, r-l+1)
+        
         return max_len
+        
             
 
 
