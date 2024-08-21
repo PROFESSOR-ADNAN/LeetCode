c0268c293826baf33a792aa5a2f0125cc2be43c9
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-21 18:33:31 +0300

    Time: 545 ms (30.89%) | Memory: 24.5 MB (72.05%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,16 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        curr_max = nums[0]
-        max_sofar = nums[0]
-
+        curr_sum = nums[0]
+        max_sum = curr_sum
         for i in range(1, len(nums)):
-            if nums[i] > nums[i] + curr_max:
-                curr_max = nums[i]
-            else:
-                curr_max = curr_max + nums[i]
-            
-            if curr_max > max_sofar:
-                max_sofar = curr_max
+            curr_sum = max(nums[i], nums[i]+curr_sum)
+            max_sum = max(max_sum, curr_sum)
 
-        return max_sofar
+        return max_sum
+
+
+
+
+
+
+
+
+
+
+        # curr_max = nums[0]
+        # max_sofar = nums[0]
+
+        # for i in range(1, len(nums)):
+        #     if nums[i] > nums[i] + curr_max:
+        #         curr_max = nums[i]
+        #     else:
+        #         curr_max = curr_max + nums[i]
+            
+        #     if curr_max > max_sofar:
+        #         max_sofar = curr_max
+
+        # return max_sofar
