d5e6a86720a9ff46c29355a7e2de09bd0f2e4017
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-21 18:34:59 +0300

    Time: 541 ms (37.29%) | Memory: 24.8 MB (15.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -6,32 +6,9 @@ class Solution(object):
         """
         curr_sum = nums[0]
         max_sum = curr_sum
-        
+
         for i in range(1, len(nums)):
             curr_sum = max(nums[i], nums[i]+curr_sum)
             max_sum = max(max_sum, curr_sum)
 
         return max_sum
-
-
-
-
-
-
-
-
-
-
-        # curr_max = nums[0]
-        # max_sofar = nums[0]
-
-        # for i in range(1, len(nums)):
-        #     if nums[i] > nums[i] + curr_max:
-        #         curr_max = nums[i]
-        #     else:
-        #         curr_max = curr_max + nums[i]
-            
-        #     if curr_max > max_sofar:
-        #         max_sofar = curr_max
-
-        # return max_sofar
