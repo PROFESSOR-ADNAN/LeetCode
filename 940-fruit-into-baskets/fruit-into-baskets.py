2c7a916538cf0e99065d6e4ae149e4538800a982
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-21 19:21:20 +0300

    Time: 658 ms (83.33%) | Memory: 17.4 MB (61.04%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -9,11 +9,12 @@ class Solution(object):
         l = 0
         for r in range(len(fruits)):
             fruits_type[fruits[r]] = 1 + fruits_type.get(fruits[r], 0)
-            while len(fruits_type) > 2:
+            if len(fruits_type) > 2:
                 fruits_type[fruits[l]] -= 1
                 if fruits_type[fruits[l]] == 0:
                     del fruits_type[fruits[l]]
                 l += 1
-            max_len = max(max_len, r-l+1)
+            if len(fruits_type) <= 2:
+                max_len = max(max_len, r-l+1)
 
         return max_len
