91a9b1f60471995d4d76379615fab6cf21dfb3c3
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-22 09:58:40 +0300

    Time: 63 ms (70.01%) | Memory: 11.9 MB (84.84%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        max_len = 0
+        max_f = 0
+        ch = {}
+        l, r = 0, 0
+        while r < len(s):
+            ch[s[r]] = 1 + ch.get(s[r], 0)
+            max_f = max(max_f, ch[s[r]])
+            while (r-l+1) - max_f > k:
+                ch[s[l]] -= 1
+                l += 1
+            max_len = max(max_len, (r-l+1))
+            r += 1
+        
+        return max_len
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # l, r = 0, 0
+        # substring = ""
+        # max_len = 0
+        # while r < len(s):
+        #     substring += s[r]
+        #     if len(substring) > k+1:
+        #         count = {}
+        #         count = Counter(substring)
+        #         for key, cnt in count.items():
+        #             if cnt == len(substring) - k:
+        #                 max_len = max(max_len, r-l+1)
+        #                 break
+        #     r += 1
+    
+        # return max_len
