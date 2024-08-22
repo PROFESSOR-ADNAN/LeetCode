417a3155db63c38ddba3d35a062816c60fbc2544
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-22 09:59:55 +0300

    Time: 63 ms (70.01%) | Memory: 12 MB (69.11%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -12,10 +12,11 @@ class Solution(object):
         while r < len(s):
             ch[s[r]] = 1 + ch.get(s[r], 0)
             max_f = max(max_f, ch[s[r]])
-            while (r-l+1) - max_f > k:
+            if (r-l+1) - max_f > k:
                 ch[s[l]] -= 1
                 l += 1
-            max_len = max(max_len, (r-l+1))
+            if (r-l+1) - max_f <= k:
+                max_len = max(max_len, (r-l+1))
             r += 1
         
         return max_len
