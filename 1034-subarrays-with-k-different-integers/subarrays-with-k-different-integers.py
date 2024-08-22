fe0f01f7dcd10632977c458116f493cac461fcf0
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-22 20:21:13 +0300

    Time: 393 ms (52.00%) | Memory: 13.8 MB (78.29%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -6,17 +6,19 @@ class Solution(object):
         :rtype: int
         """
         def func(k):
-            cnt, l = 0, 0
             mpp = {}
+            cnt, l = 0, 0
             for r in range(len(nums)):
                 mpp[nums[r]] = 1 + mpp.get(nums[r], 0)
                 while len(mpp) > k:
                     mpp[nums[l]] -= 1
                     if mpp[nums[l]] == 0:
-                        del mpp[nums[l]]
+                        mpp.pop(nums[l])
                     l += 1
-                cnt += (r-l+1)
-
+                cnt += r-l+1
+            
             return cnt
-
+        
         return func(k) - func(k-1)
+
+
