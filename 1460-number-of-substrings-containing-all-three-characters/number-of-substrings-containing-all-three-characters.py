aca369b3b395d694a3e1888814ab7a051e6a248e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-22 08:53:45 +0300

    Time: 108 ms (87.55%) | Memory: 13.6 MB (49.81%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        cnt = 0
+        ch = {}
+        for r in range(len(s)):
+            ch[s[r]] = r
+            if len(ch) == 3:
+                cnt += 1 + min(ch['a'], ch['b'], ch['c'])
+        
+        return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # l, r = 0, 0
+        # total_len = 0
+        # letters = {"a", "b", "c"}
+        # while r < len(s):
+        #     if s[r] in letters:
+        #         letters.remove(s[r])
+        #     if not letters:
+        #         total_len += len(s) - r
+        #         l += 1
+        #         r = l - 1
+        #         letters = {"a", "b", "c"}
+        #     r += 1
+
+        # return total_len
