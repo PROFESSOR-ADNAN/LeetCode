ebb2ec73361f801af8d0251bc1ac31a5432def4a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2024-08-22 13:59:11 +0300

    Time: 682 ms (14.89%) | Memory: 16.7 MB (93.02%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -6,6 +6,7 @@ class Solution(object):
         :rtype: int
         """
         def func(k):
+            
             if k < 0: return 0
             curr, cnt, l = 0, 0, 0
             for r in range(len(nums)):
@@ -15,7 +16,6 @@ class Solution(object):
                     if nums[l] % 2 == 1:
                         curr -= 1
                     l += 1
-
                 cnt += (r-l+1)
             
             return cnt
