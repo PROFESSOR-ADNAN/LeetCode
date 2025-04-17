9c6b2f84c93ae909c1e4fcfef9aa767f2feb503e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-17 17:36:32 +0300

    Time: 4 ms (38.19%) | Memory: 12.8 MB (25.82%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -5,17 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        q = deque()
-        for i in range(1, n+1):
-            q.append(i)
+        def helper(n, k):
+            if n == 1:
+                return 0
+            return (helper(n-1, k) + k) % n
+        return helper(n, k) + 1
 
-        while len(q) > 1:
-            for _ in range(k-1):
-                num = q.popleft()
-                q.append(num)
-            q.popleft()
+        # q = deque()
+        # for i in range(1, n+1):
+        #     q.append(i)
 
-        return q[0]
+        # while len(q) > 1:
+        #     for _ in range(k-1):
+        #         num = q.popleft()
+        #         q.append(num)
+        #     q.popleft()
+
+        # return q[0]
 
 
         # players = list(range(1, n+1))
