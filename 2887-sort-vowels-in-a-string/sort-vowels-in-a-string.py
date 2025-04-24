8568f53d846a00d71233cb9379c18fb4981fff1d
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-24 21:45:42 +0300

    Time: 128 ms (86.27%) | Memory: 22.7 MB (12.74%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -4,16 +4,42 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        vowel = "AEIOUaeiou"
-        vowels = [char for char in s if char in vowel]
-        vowels_sorted = sorted(vowels)
-        res = ""
-        j = 0
+        arr = list(s)
+        vowels = []
+        ind = []
         for i in range(len(s)):
-            if s[i] in vowel:
-                res += vowels_sorted[j]
-                j += 1
-            else:
-                res += s[i]
-        return res
+            if arr[i] in "aeiouAEIOU":
+                vowels.append(arr[i])
+                ind.append(i)
+
+        vowels.sort()
+        for i in range(len(vowels)):
+            arr[ind[i]] = vowels[i]
+
+        return ''.join(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # vowel = "AEIOUaeiou"
+        # vowels = [char for char in s if char in vowel]
+        # vowels_sorted = sorted(vowels)
+        # res = ""
+        # j = 0
+        # for i in range(len(s)):
+        #     if s[i] in vowel:
+        #         res += vowels_sorted[j]
+        #         j += 1
+        #     else:
+        #         res += s[i]
+        # return res
                 
