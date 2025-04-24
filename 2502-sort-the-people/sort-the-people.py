19e1467c2402b4258bf469956e8fb5116d0b3b0f
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-24 09:30:39 +0300

    Time: 329 ms (28.72%) | Memory: 12.6 MB (97.27%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -5,11 +5,32 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        
+        # bubble sort
+        # for i in range(len(names)):
+        #     for j in range(len(names)-1-i):
+        #         if heights[j] < heights[j+1]:
+        #             heights[j], heights[j+1] = heights[j+1], heights[j]
+        #             names[j], names[j+1] = names[j+1], names[j]
+
+        # return names
+
+        # selection sort
         for i in range(len(names)):
-            for j in range(len(names)-1-i):
-                if heights[j] < heights[j+1]:
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j], names[j+1] = names[j+1], names[j]
+            maxInd = i
+            for j in range(i+1, len(names)):
+                if heights[j] > heights[maxInd]:
+                    maxInd = j
+            heights[i], heights[maxInd] = heights[maxInd], heights[i]
+            names[i], names[maxInd] = names[maxInd], names[i]
 
         return names
+
+
+        # insertion sort
+
+
+        # count sort
+
+        # merge sort
+
+        # quick sort
