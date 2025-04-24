9226ba9f9bae5f258c810416e366bcc04c0dc39b
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-24 14:25:00 +0300

    Time: 19 ms (36.54%) | Memory: 12.4 MB (83.39%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -4,6 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: str
         """   
-        arr = s.split(" ")
-        arr.sort(key = lambda x: int(x[-1]))
-        return " ".join(x[:-1] for x in arr)
+        arr = list(s.split())
+        arr.sort(key = lambda x: x[-1])
+        ans = ''
+        for word in arr:
+            ans += word[:len(word)-1] + " "
+        return ans[:len(ans)-1]
+
+
+
+
+
+
+
+
+
+
+        # arr = s.split(" ")
+        # arr.sort(key = lambda x: int(x[-1]))
+        # return " ".join(x[:-1] for x in arr)
