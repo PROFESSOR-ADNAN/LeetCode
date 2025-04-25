84fe66dc9a37c43d0d399df17db3741f77828470
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-25 21:25:35 +0300

    Time: 275 ms (9.59%) | Memory: 12.5 MB (69.30%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -3,21 +3,43 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[int]
-        """
-        max_value = max(nums) 
-        min_value = min(nums)
-        range_value = max_value - min_value + 1
-
-        count = [0] * range_value
-        for num in nums:
-            count[num-min_value] += 1
-
-        for i in range(1, len(count)):
-            count[i] += count[i-1]
-
+        """ 
+        ans = []
         for i in range(len(nums)):
-            if nums[i] == min_value:
-                nums[i] = 0
-            else:
-                nums[i] = count[nums[i] - min_value - 1]
-        return nums
+            count = 0
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                if nums[i] > nums[j]:
+                    count += 1
+            ans.append(count)
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+        # max_value = max(nums) 
+        # min_value = min(nums)
+        # range_value = max_value - min_value + 1
+
+        # count = [0] * range_value
+        # for num in nums:
+        #     count[num-min_value] += 1
+
+        # for i in range(1, len(count)):
+        #     count[i] += count[i-1]
+
+        # for i in range(len(nums)):
+        #     if nums[i] == min_value:
+        #         nums[i] = 0
+        #     else:
+        #         nums[i] = count[nums[i] - min_value - 1]
+        # return nums
