642046f6be0b4a9a3826c3ccc93e16ce7bb66783
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-26 09:42:01 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (84.88%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -18,13 +18,39 @@ class Solution(object):
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
 
+
+
+
+
+
+
+
+
+
+
+        # i = m-1
+        # j = n-1
+        # k = m+n-1
+        # while i >= 0 and j >= 0:
+        #     if nums1[i] > nums2[j]:
+        #         nums1[k] = nums1[i]
+        #         i -= 1
+        #     else:
+        #         nums1[k] = nums2[j]
+        #         j -= 1
+        #     k -= 1
+
+        # while j >= 0:
+        #     nums1[k] = nums2[j]
+        #     j -= 1
+        #     k -= 1
+
         
-        # for i in range(n):
-        #     nums1[m+i] = nums2[i]
-        # nums1.sort()
+        # # for i in range(n):
+        # #     nums1[m+i] = nums2[i]
+        # # nums1.sort()
