5be3dbc5b6dabf07fecb902dfe7aff5099c3042b
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-26 10:31:56 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (69.47%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.py
@@ -5,25 +5,3 @@ class Solution(object):
         :rtype: bool
         """
         return False
-
-        # def isPalindromic(num):
-        #     l, r = 0, len(num)-1
-        #     while l < r:
-        #         if num[l] != num[r]:
-        #             return False
-        #         l += 1
-        #         r -= 1
-        #     return True
-
-        # for i in range(2, n-1):
-        #     curr = n
-        #     currStr = ''
-        #     while curr:
-        #         r = curr % i
-        #         curr = curr // i
-        #         currStr += str(r)
-
-        #     if not isPalindromic(currStr):
-        #         return False
-
-        # return True
