4be1b30455ed2575a933c9f637fe75dfb2416636
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-26 09:57:55 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (89.04%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -4,36 +4,32 @@ class Solution(object):
         :type image: List[List[int]]
         :rtype: List[List[int]]
         """
-        R = len(image)
-        C = len(image[0])
-        for r in range(R):
-            for c in range(C):
-                if image[r][c] == 0:
-                    image[r][c] = 1
-                else:
-                    image[r][c] = 0
-        for r in range(R):
-            row = image[r]
-            l, r = 0, C-1
-            while l < r:
-                row[l], row[r] = row[r], row[l]
+        for row in image:
+            l = 0
+            r = len(row)-1
+            while l <= r:
+                row[l], row[r] = 1 - row[r], 1 - row[l]
                 l += 1
                 r -= 1
         return image
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        # R = len(image)
+        # C = len(image[0])
+        # for r in range(R):
+        #     for c in range(C):
+        #         if image[r][c] == 0:
+        #             image[r][c] = 1
+        #         else:
+        #             image[r][c] = 0
+        # for r in range(R):
+        #     row = image[r]
+        #     l, r = 0, C-1
+        #     while l < r:
+        #         row[l], row[r] = row[r], row[l]
+        #         l += 1
+        #         r -= 1
+        # return image
 
 
         # for row in image:
