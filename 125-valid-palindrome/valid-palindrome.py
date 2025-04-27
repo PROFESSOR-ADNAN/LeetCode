d20162ca2e1178eb19409c77715b519653987e21
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-27 21:25:54 +0300

    Time: 319 ms (5.02%) | Memory: 16.2 MB (5.17%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def isPalindrome(s):
+            l, r = 0, len(s)-1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        newS = ''
+        for i in range(len(s)):
+            if (ord(s[i]) in range(ord('0'), ord('9')+1) or
+                ord(s[i]) in range(ord('A'), ord('Z')+1) or
+                ord(s[i]) in range(ord('a'), ord('z')+1)):
+                newS += s[i].lower()
+
+        return isPalindrome(newS)
+
+
+
+
+
+        # only_alphanumeric = []
+        # for elem in s.lower().replace(" ", ""):
+        #     if (ord(elem) >= 65 and ord(elem) <= 90) or (ord(elem) >= 97 and ord(elem) <= 122):
+        #         only_alphanumeric.append(elem)
+        # left = 0
+        # right = len(only_alphanumeric) - 1
+        # while left <= right:
+        #     if only_alphanumeric[left] != only_alphanumeric[right]:
+        #         return False
+        #     else:
+        #         left += 1
+        #         right -= 1
+        # return True
+        
