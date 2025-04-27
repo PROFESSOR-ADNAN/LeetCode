8681127f64e55c7e28e32db2a7082538df7c5c1c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-27 22:46:20 +0300

    Time: 703 ms (69.75%) | Memory: 18.4 MB (63.26%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -8,6 +8,9 @@ class Solution(object):
         ans = []
 
         for k in range(len(nums)):
+            if k and nums[k] == nums[k-1]:
+                continue
+
             l, r = k + 1, len(nums) - 1
             while l < r:
                 s = nums[k] + nums[l] + nums[r]
@@ -16,14 +19,35 @@ class Solution(object):
                 elif s > 0:
                     r -= 1
                 else:
-                    if [nums[k], nums[l], nums[r]] not in ans:
-                        ans.append([nums[k], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-        
+                    ans.append([nums[k], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]: l += 1
+                    while l < r and nums[r] == nums[r-l]: r -= 1
+                    l, r = l+1, r-1
+
         return ans
 
 
+
+
+
+        # nums.sort()
+        # ans = []
+ 
+        # for k in range(len(nums)):
+        #     l, r = k + 1, len(nums) - 1
+        #     while l < r:
+        #         s = nums[k] + nums[l] + nums[r]
+        #         if s < 0:
+        #             l += 1
+        #         elif s > 0:
+        #             r -= 1
+        #         else:
+        #             if [nums[k], nums[l], nums[r]] not in ans:
+        #                 ans.append([nums[k], nums[l], nums[r]])
+        #             l += 1
+        #             r -= 1
+        
+        # return ans
 
 
 
