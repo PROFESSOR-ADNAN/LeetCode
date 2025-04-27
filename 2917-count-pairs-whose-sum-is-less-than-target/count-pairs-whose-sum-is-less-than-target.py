33094d12f5173476fa4fb6f2422bde6ab1dc0fc9
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-27 22:21:10 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (0.16%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
@@ -5,10 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        nums.sort()
         count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] < target:
-                    count += 1
-        
+        l, r = 0, len(nums)-1
+        while l < r:
+            s = nums[l] + nums[r]
+            if s < target:
+                count += (r-l)
+                l += 1
+            else:
+                r -= 1
+
         return count
+
+
+
+
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] < target:
+        #             count += 1
+        
+        # return count
