db27989b8fb03f34da2529703bbc864ac2030527
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-27 21:40:50 +0300

    Time: 3 ms (62.44%) | Memory: 12.6 MB (60.49%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -5,14 +5,33 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        count1 = Counter(nums1)
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
         ans = []
-        for i in range(len(nums2)):
-            if nums2[i] in count1 and count1[nums2[i]] > 0:
-                ans.append(nums2[i])
-                count1[nums2[i]] -= 1
-        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
         return ans
+
+
+
+
+
+        # count1 = Counter(nums1)
+        # ans = []
+        # for i in range(len(nums2)):
+        #     if nums2[i] in count1 and count1[nums2[i]] > 0:
+        #         ans.append(nums2[i])
+        #         count1[nums2[i]] -= 1
+        
+        # return ans
 
 
 
