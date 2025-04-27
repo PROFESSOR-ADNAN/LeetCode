f7791514f0f85eb34386eec589fc11f5cca6ed30
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-27 18:29:32 +0300

    Time: 2 ms (69.11%) | Memory: 13.9 MB (35.78%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,26 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        newNums= []
-        for i in range(len(nums)):
-            if nums[i] not in newNums:
-                newNums.append(nums[i])
+        ind = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[ind] = nums[i]
+                ind += 1
+        return ind
+
+        # newNums= []
+        # for i in range(len(nums)):
+        #     if nums[i] not in newNums:
+        #         newNums.append(nums[i])
         
-        for i in range(len(newNums)):
-            nums[i] = newNums[i]
+        # for i in range(len(newNums)):
+        #     nums[i] = newNums[i]
         
-        return len(newNums)
-
-
-
-
-
-
+        # return len(newNums)
 
 
         # index = 1
         # n = len(nums)
-        # for i in range(1, n):dd
+        # for i in range(1, n):
         #     if nums[i] != nums[i-1]:
         #         nums[index] = nums[i]
         #         index += 1
