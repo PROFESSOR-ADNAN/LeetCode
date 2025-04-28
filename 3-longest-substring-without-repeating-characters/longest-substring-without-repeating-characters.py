b87b77e4c5271cb5b6995747d902f216661146fa
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-28 13:14:31 +0300

    Time: 3172 ms (5.80%) | Memory: 14.3 MB (5.85%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,16 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        max_len = 0
-        ch = {}
-        l = 0
-        r = 0
-        while r < len(s):
-            if s[r] in ch:
-                if ch[s[r]] >= l:
-                    l = ch[s[r]] + 1
-            max_len = max(max_len, r-l+1)
-            ch[s[r]] = r
-            r += 1
+        maxCount = 0
+        for i in range(len(s)):
+            count = 0
+            unique = set()
+            for j in range(i, len(s)):
+                if s[j] not in unique:
+                    unique.add(s[j])
+                    count += 1
+                else:
+                    break
+            maxCount = max(maxCount, count)
 
-        return max_len
+        return maxCount
+
+
+
+
+        # max_len = 0
+        # ch = {}
+        # l = 0
+        # r = 0
+        # while r < len(s):
+        #     if s[r] in ch:
+        #         if ch[s[r]] >= l:
+        #             l = ch[s[r]] + 1
+        #     max_len = max(max_len, r-l+1)
+        #     ch[s[r]] = r
+        #     r += 1
+
+        # return max_len
