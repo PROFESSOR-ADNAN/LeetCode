b9da95dffae46987d4aed8401dfbbea3a2595e5c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-28 12:17:41 +0300

    Time: 180 ms (12.77%) | Memory: 18.5 MB (96.06%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -6,14 +6,51 @@ class Solution(object):
         :rtype: float
         """
         l = 0
+        window = 0
+        for i in range(k):
+            window += nums[i]
         r = k
-        curr_max = sum(nums[:k])
-        maximum = curr_max
+        maxAverage = float('-inf')
+        maxAverage = max(maxAverage, float(window) / float(k))
         while r < len(nums):
-            curr_max -= nums[l]
-            curr_max += nums[r]
-            l += 1
+            window += nums[r]
+            window -= nums[l]
             r += 1
-            if curr_max > maximum:
-                maximum = curr_max
-        return (maximum/(k * 1.0))
+            l += 1
+            
+            maxAverage = max(maxAverage, float(window) / float(k))
+
+        
+        return maxAverage
+
+
+
+
+        # maxVal = -1 * float('inf')
+        # for i in range(len(nums)-k+1):
+        #     temp = 0
+        #     for j in range(i, i+k):
+        #         temp += nums[j]
+        #     maxVal = max(maxVal, temp)
+
+        # return float(maxVal) / float(k)
+
+
+
+
+
+
+
+
+        # l = 0
+        # r = k
+        # curr_max = sum(nums[:k])
+        # maximum = curr_max
+        # while r < len(nums):
+        #     curr_max -= nums[l]
+        #     curr_max += nums[r]
+        #     l += 1
+        #     r += 1
+        #     if curr_max > maximum:
+        #         maximum = curr_max
+        # return (maximum/(k * 1.0))
