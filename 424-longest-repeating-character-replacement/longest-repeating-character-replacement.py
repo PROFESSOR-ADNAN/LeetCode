6c755d083e2633642b8c860babccbb6acf0e18d0
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-28 17:27:45 +0300

    Time: 150 ms (52.73%) | Memory: 13 MB (80.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,23 +6,43 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, 0
+        count = {}
         maxL = 0
         maxF = 0
-        count = {}
         while r < len(s):
             count[s[r]] = 1 + count.get(s[r], 0)
-            for value in count.values():
-                maxF = max(maxF, value)
-            while (r-l+1) - maxF > k:
+            maxF = max(maxF, count[s[r]])
+            if (r-l+1) - maxF > k:
                 count[s[l]] -= 1
                 l += 1
-            for value in count.values():
-                maxF = max(maxF, value)
-            
-            maxL = max(maxL, r-l+1)
+            if (r-l+1) - maxF <= k:
+                maxL = max(maxL, r-l+1)
             r += 1
         
         return maxL
+
+
+
+
+
+        # l, r = 0, 0
+        # maxL = 0
+        # maxF = 0
+        # count = {}
+        # while r < len(s):
+        #     count[s[r]] = 1 + count.get(s[r], 0)
+        #     for value in count.values():
+        #         maxF = max(maxF, value)
+        #     while (r-l+1) - maxF > k:
+        #         count[s[l]] -= 1
+        #         l += 1
+        #     for value in count.values():
+        #         maxF = max(maxF, value)
+            
+        #     maxL = max(maxL, r-l+1)
+        #     r += 1
+        
+        # return maxL
             
 
 
@@ -43,13 +63,6 @@ class Solution(object):
         #             maxL = max(maxL, j-i+1)
 
         # return maxL
-
-
-
-
-
-
-
 
 
 
