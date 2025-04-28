ca12975fd8278b551efa55a1784c9b3c3483cf40
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-04-28 15:41:40 +0300

    Time: 522 ms (5.32%) | Memory: 12.9 MB (80.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,21 +5,44 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        max_len = 0
-        max_f = 0
-        ch = {}
         l, r = 0, 0
+        maxL = 0
+        maxF = 0
+        count = {}
         while r < len(s):
-            ch[s[r]] = 1 + ch.get(s[r], 0)
-            max_f = max(max_f, ch[s[r]])
-            if (r-l+1) - max_f > k:
-                ch[s[l]] -= 1
+            count[s[r]] = 1 + count.get(s[r], 0)
+            for value in count.values():
+                maxF = max(maxF, value)
+            while (r-l+1) - maxF > k:
+                count[s[l]] -= 1
                 l += 1
-            if (r-l+1) - max_f <= k:
-                max_len = max(max_len, (r-l+1))
+            for value in count.values():
+                maxF = max(maxF, value)
+            
+            maxL = max(maxL, r-l+1)
             r += 1
         
-        return max_len
+        return maxL
+            
+
+
+
+
+        # maxL = 0
+        # for i in range(len(s)):
+        #     count = {}
+        #     for j in range(i, len(s)):
+        #         count[s[j]] = 1 + count.get(s[j], 0)
+                
+        #         highest = 0
+        #         for key, value in count.items():
+        #             if value > highest:
+        #                 highest = value
+                
+        #         if (j-i+1) - highest <= k:
+        #             maxL = max(maxL, j-i+1)
+
+        # return maxL
 
 
 
@@ -32,24 +55,35 @@ class Solution(object):
 
 
 
-
-
-
-
-
-
-        # l, r = 0, 0
-        # substring = ""
         # max_len = 0
+        # max_f = 0
+        # ch = {}
+        # l, r = 0, 0
         # while r < len(s):
-        #     substring += s[r]
-        #     if len(substring) > k+1:
-        #         count = {}
-        #         count = Counter(substring)
-        #         for key, cnt in count.items():
-        #             if cnt == len(substring) - k:
-        #                 max_len = max(max_len, r-l+1)
-        #                 break
+        #     ch[s[r]] = 1 + ch.get(s[r], 0)
+        #     max_f = max(max_f, ch[s[r]])
+        #     if (r-l+1) - max_f > k:
+        #         ch[s[l]] -= 1
+        #         l += 1
+        #     if (r-l+1) - max_f <= k:
+        #         max_len = max(max_len, (r-l+1))
         #     r += 1
-    
+        
         # return max_len
+
+
+        # # l, r = 0, 0
+        # # substring = ""
+        # # max_len = 0
+        # # while r < len(s):
+        # #     substring += s[r]
+        # #     if len(substring) > k+1:
+        # #         count = {}
+        # #         count = Counter(substring)
+        # #         for key, cnt in count.items():
+        # #             if cnt == len(substring) - k:
+        # #                 max_len = max(max_len, r-l+1)
+        # #                 break
+        # #     r += 1
+    
+        # # return max_len
