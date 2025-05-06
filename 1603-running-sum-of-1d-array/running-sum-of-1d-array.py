06e3d60553b6f0ecb95451c43e1147279ab26be8
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-05-06 18:24:51 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (31.96%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        sum = 0
-        res = []
-        for i in range(len(nums)):
-            sum += nums[i]
-            res.append(sum)
-        return res
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+
+
+
+
+
+        # sum = 0
+        # res = []
+        # for i in range(len(nums)):
+        #     sum += nums[i]
+        #     res.append(sum)
+        # return res
