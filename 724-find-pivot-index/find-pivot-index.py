ff44bdfcb69baafb92278ad7671bbedaffdc9734
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-05-08 18:02:47 +0300

    Time: 5 ms (68.59%) | Memory: 13.4 MB (55.50%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -4,15 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        Rprefix = nums[:]
+        total = sum(nums)
+        left = 0
 
-        for i in range(len(nums)-2, -1, -1):
-            Rprefix[i] += Rprefix[i+1]
-
-        for i in range(1, len(nums)):
-            nums[i] += nums[i-1]
-
-        for i in range(len(nums)):
-            if Rprefix[i] == nums[i]:
+        for i, num in enumerate(nums):
+            total -= num
+            if total == left:
                 return i
+            left += num
         return -1
+        
+        # Rprefix = nums[:]
+
+        # for i in range(len(nums)-2, -1, -1):
+        #     Rprefix[i] += Rprefix[i+1]
+
+        # for i in range(1, len(nums)):
+        #     nums[i] += nums[i-1]
+
+        # for i in range(len(nums)):
+        #     if Rprefix[i] == nums[i]:
+        #         return i
+        # return -1
