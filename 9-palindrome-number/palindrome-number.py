ce1a6ba28b590fc1d4eef2864c4186b92d23e1de
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-07-09 05:34:41 +0300

    Time: 22 ms (14.60%) | Memory: 12.5 MB (10.55%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -7,18 +7,45 @@ class Solution(object):
         if x < 0: return False
 
         div = 1
-        while x >= 10 * div:
+        while x >= div * 10:
             div *= 10
-        
+
         while x:
+            left = x // div 
             right = x % 10
-            left = x // div
-
-            if right != left: return False
-
-            x = (x % div) // 10
-            div /= 100
             
+            if right != left:
+                return False
+            
+            x = (x % div) // 10
+            div //= 100
         return True
+
+
+
+
+
+
+
+
+
+
+
+        # if x < 0: return False
+
+        # div = 1
+        # while x >= 10 * div:
+        #     div *= 10
+        
+        # while x:
+        #     right = x % 10
+        #     left = x // div
+
+        #     if right != left: return False
+
+        #     x = (x % div) // 10
+        #     div /= 100
+            
+        # return True
             
         
