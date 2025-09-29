d877d542ff5d9addc1b01d7523bf4312af2d7a85
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-09-29 05:04:21 +0300

    Time: 16 ms (33.98%) | Memory: 19.5 MB (75.50%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -6,51 +6,16 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, 0
-        curr = 0
-        minLen = float('inf')
+        sum_ = 0
+        minLen = float("inf")
+
         while r < len(nums):
-            curr += nums[r]
-            while curr >= target:
-                if (r-l+1) < minLen:
-                    minLen = r-l+1
-                curr -= nums[l]
+            sum_ += nums[r]
+
+            while sum_ >= target:
+                minLen = min(minLen, r-l+1)
+                sum_ -= nums[l]
                 l += 1
             r += 1
-        
-        return minLen if minLen != float('inf') else 0
 
-
-
-        # minLen = float('inf')
-        # for i in range(len(nums)):
-        #     curr = 0
-        #     for j in range(i, len(nums)):
-        #         curr += nums[j]
-        #         if curr >= target:
-        #             if (j-i+1) < minLen:
-        #                 minLen = j-i+1
-        #                 break
-        
-        # return minLen if minLen != float('inf') else 0
-
-
-
-
-
-
-
-
-
-        # start = 0
-        # sum = 0
-        # end = 0
-        # result = len(nums)
-        # while end < len(nums):
-        #     sum += nums[end]
-        #     end += 1
-        #     while sum >= target:
-        #         result = min(result, end - start)
-        #         sum -= nums[start]
-        #         start += 1
-        # return result
-        
+        return minLen if minLen != float("inf") else 0
