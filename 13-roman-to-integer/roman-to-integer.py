aafa028f5769bbfe1cf4dc7a625027beeddbd876
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-10-01 04:16:48 +0300

    Time: 11 ms (43.88%) | Memory: 12.3 MB (84.32%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,14 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        ans = 0
-        n = len(s)
+        romanToInt = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        for i in range(n):
-            if i < n - 1 and roman[s[i]] < roman[s[i+1]]:
-                ans -= roman[s[i]]
+        num = 0
+        for i in range(len(s)):
+            if s[i] == "I":
+                if i+1 < len(s) and (s[i+1] == "V" or s[i+1] == "X"):
+                    num -= romanToInt[s[i]]
+                else:
+                    num += romanToInt[s[i]]
+            elif s[i] == "X":
+                if i+1 < len(s) and (s[i+1] == "L" or s[i+1] == "C"):
+                    num -= romanToInt[s[i]]
+                else: 
+                    num += romanToInt[s[i]]
+            elif s[i] == "C":
+                if i+1 < len(s) and (s[i+1] == "D" or s[i+1] == "M"):
+                    num -= romanToInt[s[i]]
+                else:
+                    num += romanToInt[s[i]]
             else:
-                ans += roman[s[i]]
-        
-        return ans
+                num += romanToInt[s[i]]
+
+        return num
