bba162c46a3685e5c1e420aaca85241a017e8db5
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-11-23 11:01:53 +0300

    Time: 3 ms (88.95%) | Memory: 13.6 MB (60.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,43 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i, j = 0, 0
-        while j < len(nums):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-            j += 1
+        ind = 0
+        for search in range(len(nums)):
+            if nums[search] != 0:
+                nums[search], nums[ind] = nums[ind], nums[search]
+                ind += 1
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        # ind = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[i], nums[ind] = nums[ind], nums[i]
+        #         ind += 1
 
-        return nums
+
+        # return nums
+
+
+        
+        
+        
+        # i, j = 0, 0
+        # while j < len(nums):
+        #     if nums[j] != 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         i += 1
+        #     j += 1
+
+        # return nums
         
