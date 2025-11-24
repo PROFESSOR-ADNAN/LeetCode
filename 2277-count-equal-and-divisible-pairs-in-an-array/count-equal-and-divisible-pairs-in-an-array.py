c97894f3d3d23c74f312aeef82f98a88ef5d1bc5
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-11-24 03:27:30 +0300

    Time: 4 ms (100.00%) | Memory: 12.3 MB (98.59%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -5,10 +5,25 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        Map = {}
         count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j] and (i*j) % k == 0:
-                    count += 1
+        for i, num in enumerate(nums):
+            if num in Map:
+                for ind in Map[num]:
+                    if (ind * i) % k == 0:
+                        count += 1
+                Map[num].append(i)
+            else:
+                Map[num] = [i]
         return count
+
+
+
+
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j] and (i*j) % k == 0:
+        #             count += 1
+        # return count
         
