a017484f8b2d10ea14f6d655eae9d0b4c6307ad1
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-11-27 15:14:20 +0300

    Time: 15 ms (9.98%) | Memory: 26.1 MB (7.39%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,9 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        rotate = nums[len(nums)-(k%len(nums)):]
-        rotate.extend(nums[:len(nums)-(k%len(nums))])
-        for i in range(len(rotate)):
-            nums[i] = rotate[i]
+        k = k % len(nums)
+        q = deque(nums)
+
+        for i in range(len(nums)-k):
+            q.append(q.popleft())
+        for i in range(len(nums)):
+            nums[i] = q.popleft()
+            
+
+
+
+        # rotate = nums[len(nums)-(k%len(nums)):]
+        # rotate.extend(nums[:len(nums)-(k%len(nums))])
+        # for i in range(len(rotate)):
+        #     nums[i] = rotate[i]
             
 
