f06d2c8edbe5755d11e44e9d4946e54aaa187276
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-11-30 09:03:10 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (98.81%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -5,20 +5,44 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: List[int]
         """
-        arr2ElementsRelativeOrder = {}
-        for i in range(len(arr2)):
-            arr2ElementsRelativeOrder[arr2[i]] = i
+        max_ = max(arr1)
+        count = [0] * (max_ + 1)
 
-        arr1.sort()
-        for i in range(len(arr1)):
-            if arr1[i] not in arr2ElementsRelativeOrder:
-                arr2ElementsRelativeOrder[arr1[i]] = i + len(arr2)
+        for val in arr1:
+            count[val] += 1
+        
+        res = [0] * len(arr1)
+        ind = 0
+        for val in arr2:
+            while count[val] > 0:
+                res[ind] = val
+                ind += 1
+                count[val] -= 1
+        
+        for index, value in enumerate(count):
+            for i in range(value):
+                res[ind] = index
+                ind += 1
+        
+        return res
 
-        def customeComparator(item):
-            return arr2ElementsRelativeOrder[item]
 
-        arr1.sort(key = customeComparator)
-        return arr1
+
+
+        # arr2ElementsRelativeOrder = {}
+        # for i in range(len(arr2)):
+        #     arr2ElementsRelativeOrder[arr2[i]] = i
+
+        # arr1.sort()
+        # for i in range(len(arr1)):
+        #     if arr1[i] not in arr2ElementsRelativeOrder:
+        #         arr2ElementsRelativeOrder[arr1[i]] = i + len(arr2)
+
+        # def customeComparator(item):
+        #     return arr2ElementsRelativeOrder[item]
+
+        # arr1.sort(key = customeComparator)
+        # return arr1
 
 
         
