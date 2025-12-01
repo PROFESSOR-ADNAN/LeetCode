8da28e8f7101695f1625303b3dfcee3084f546c8
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-01 09:30:25 +0300

    Time: 84 ms (86.67%) | Memory: 30.6 MB (43.33%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -6,19 +6,71 @@ class Solution(object):
         :rtype: str
         """
         temp = [0] * (len(s) + 1)
-        for st, e, d in shifts:
-            if d:
-                temp[st] += 1
-                temp[e+1] -= 1
+        for l, r, k in shifts:
+            if k == 1:
+                temp[l] += 1
+                temp[r+1] -= 1
             else:
-                temp[st] -= 1
-                temp[e+1] += 1
+                temp[l] -= 1
+                temp[r+1] += 1
+
         for i in range(1, len(temp)):
             temp[i] += temp[i-1]
-        
-        new_s = ''
+        newStr = ""
         for i in range(len(s)):
-            new_s += chr(((ord(s[i]) - ord('a') + temp[i]) % 26 ) + ord('a')
-)
-        return new_s
+            newStr += chr(((ord(s[i]) - ord("a") + temp[i]) % 26) + ord("a"))
+
+        return newStr
+
+
+
+        # asciiS = [ord(char) for char in s]
+        # temp = [0] * len(s)
+
+        # for l, r, k in shifts:
+        #     if k == 0:
+        #         temp[l] += -1
+        #         if r+1 < len(temp):
+        #             temp[r+1] += 1
+        #     else:
+        #         temp[l] += 1
+        #         if r+1 < len(temp):
+        #             temp[r+1] += -1
+
+        # for i in range(1, len(temp)):
+        #     temp[i] += temp[i-1]
+
+        # for i in range(len(temp)):
+        #     res = asciiS[i] + temp[i]
+        #     if res < ord("a"):
+        #         res = ord("z") - (ord("a")-res) + 1
+        #     elif res > ord("z"):
+        #         res = ord("a") + (res-ord("z")) - 1
+        #     asciiS[i] = res
+
+
+        # ans = ""
+        # for i in range(len(asciiS)):
+        #     ans += chr(asciiS[i])
+
+        # return ans
+            
+
+
+#         temp = [0] * (len(s) + 1)
+#         for st, e, d in shifts:
+#             if d:
+#                 temp[st] += 1
+#                 temp[e+1] -= 1
+#             else:
+#                 temp[st] -= 1
+#                 temp[e+1] += 1
+#         for i in range(1, len(temp)):
+#             temp[i] += temp[i-1]
+        
+#         new_s = ''
+#         for i in range(len(s)):
+#             new_s += chr(((ord(s[i]) - ord('a') + temp[i]) % 26 ) + ord('a')
+# )
+#         return new_s
         
