19e6332fd29062d188787c62cf90e51371a6f08d
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-01 17:10:26 +0300

    Time: 73 ms (48.79%) | Memory: 17.3 MB (77.54%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -1,19 +1,39 @@
 class RecentCounter(object):
 
     def __init__(self):
-        self.recent = deque()
+        self.queue = []
 
     def ping(self, t):
         """
         :type t: int
         :rtype: int
         """
-        while self.recent and t - self.recent[0] > 3000:
-                self.recent.popleft()
-        self.recent.append(t)
-        return len(self.recent)
-
+        self.queue.append(t)
+        while t - self.queue[0] > 3000:
+            self.queue.pop(0)
+        return len(self.queue)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
+
+
+# class RecentCounter(object):
+
+#     def __init__(self):
+#         self.recent = deque()
+
+#     def ping(self, t):
+#         """
+#         :type t: int
+#         :rtype: int
+#         """
+#         while self.recent and t - self.recent[0] > 3000:
+#                 self.recent.popleft()
+#         self.recent.append(t)
+#         return len(self.recent)
+
+
+# # Your RecentCounter object will be instantiated and called as such:
+# # obj = RecentCounter()
+# # param_1 = obj.ping(t)
