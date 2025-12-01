aaa3bf2a716b33474d167c5fe3fe1162ffa64251
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-01 03:27:28 +0300

    Time: 4 ms (78.70%) | Memory: 16.2 MB (56.30%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -8,18 +8,21 @@ class NumArray(object):
         for i in range(1, len(nums)):
             nums[i] += nums[i-1]
         
-
-
     def sumRange(self, left, right):
         """
         :type left: int
         :type right: int
         :rtype: int
         """
-        # return sum(self.nums[left:right+1])
-        if left:
+        if left-1 >= 0:
             return self.nums[right] - self.nums[left-1]
-        return self.nums[right]
+        else:
+            return self.nums[right]
+
+        # # return sum(self.nums[left:right+1])
+        # if left:
+        #     return self.nums[right] - self.nums[left-1]
+        # return self.nums[right]
         
    
         
