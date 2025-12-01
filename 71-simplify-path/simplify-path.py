e6c44872df628724ba4c38b1ae5c2d191b98a9d6
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-01 14:10:32 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (15.51%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -4,40 +4,48 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
+        path = list(path.split("/"))
         stack = []
-        i = 0
-        while i < len(path):
-            while i < len(path) and path[i] == "/":
-                i += 1
-                continue
-            cur = ""
-            while i < len(path) and path[i] != "/":
-                cur += path[i]
-                i += 1
-            if cur == "..":
+
+        for path in path:
+            if path == "..":
                 if stack:
                     stack.pop()
-                else:
-                    continue
-            elif cur == ".":
+            elif path == ".":
                 continue
-            elif cur:
-                stack.append(cur)
-
+            elif path:
+                stack.append(path)
         ans = "/"
         for path in stack:
             ans += path + "/"
 
         return ans[:-1] if len(ans) > 1 else ans
 
+        # stack = []
+        # i = 0
+        # while i < len(path):
+        #     while i < len(path) and path[i] == "/":
+        #         i += 1
+        #         continue
+        #     cur = ""
+        #     while i < len(path) and path[i] != "/":
+        #         cur += path[i]
+        #         i += 1
+        #     if cur == "..":
+        #         if stack:
+        #             stack.pop()
+        #         else:
+        #             continue
+        #     elif cur == ".":
+        #         continue
+        #     elif cur:
+        #         stack.append(cur)
 
+        # ans = "/"
+        # for path in stack:
+        #     ans += path + "/"
 
-
-
-
-
-
-
+        # return ans[:-1] if len(ans) > 1 else ans
 
         # arr = list(path.split(" / "))
         # print(arr)
