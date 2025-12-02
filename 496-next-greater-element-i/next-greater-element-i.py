709c2e1ff4bcef07fb6f6af6694218e65a32289e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-02 06:49:33 +0300

    Time: 0 ms (100.00%) | Memory: 12.7 MB (16.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -5,17 +5,29 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        ans = []
-        for i in range(len(nums1)):
-            j = 0
-            while nums2[j] != nums1[i]:
-                j += 1
-            
-            for k in range(j+1, len(nums2)):
-                if nums2[k] > nums1[i]:
-                    ans.append(nums2[k])
-                    break
-            else:
-                ans.append(-1)
+        stack = []
+        nextGreaterElement = {}
+        for num in nums2[::-1]:
+            while stack and stack[-1] <= num:
+                stack.pop()
+            nextGreaterElement[num] = -1 if not stack else stack[-1]
+            stack.append(num)  
 
-        return ans
+        return [nextGreaterElement[num] for num in nums1] 
+
+        
+        
+        # ans = []
+        # for i in range(len(nums1)):
+        #     j = 0
+        #     while nums2[j] != nums1[i]:
+        #         j += 1
+            
+        #     for k in range(j+1, len(nums2)):
+        #         if nums2[k] > nums1[i]:
+        #             ans.append(nums2[k])
+        #             break
+        #     else:
+        #         ans.append(-1)
+
+        # return ans
