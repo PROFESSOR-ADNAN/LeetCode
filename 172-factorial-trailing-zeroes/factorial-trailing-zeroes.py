4d7aadc843201ba23f6febedf0cf7b6662341837
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2025-12-26 04:02:49 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.52%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -6,11 +6,40 @@ class Solution(object):
         """
         trailing_zeros = 0
         divisor = 5
+
         while n >= divisor:
-            trailing_zeros += n // divisor
+            trailing_zeros += (n // divisor)
             divisor *= 5
 
         return trailing_zeros
+
+
+        # def factorial(n):
+        #     if n == 1 or n == 0:
+        #         return 1
+        #     return n * factorial(n-1)
+
+        # n_factorial = factorial(n)
+
+        # trailing_zeros = 0
+        # while n_factorial:
+        #     last_digit = n_factorial % 10
+        #     if last_digit == 0:
+        #         trailing_zeros += 1
+        #     else:
+        #         break
+        #     n_factorial /= 10
+
+        # return trailing_zeros
+
+
+        # trailing_zeros = 0
+        # divisor = 5
+        # while n >= divisor:
+        #     trailing_zeros += n // divisor
+        #     divisor *= 5
+
+        # return trailing_zeros
 
 
     #     result = self.factorial(n)
