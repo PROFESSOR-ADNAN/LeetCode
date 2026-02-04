8f33ef3f42c8ab5db4967d1d4a3bda21dbba1316
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-02-04 07:54:12 +0300

    Time: 2 ms (90.74%) | Memory: 19.4 MB (10.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,81 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """ 
-        num = x
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         div = 1
-        while num > 9:
+        while div * 10 < x:
             div *= 10
-            num //= 10
-        
         while x:
             last_digit = x % 10
             first_digit = x // div
-
-            if first_digit != last_digit:
+            if last_digit != first_digit:
                 return False
-
             x %= div
             x //= 10
             div //= 100
-
+        
         return True
 
-            
-
-
-
-
-
-
-
-
-
-
-        # if x < 0: return False
-
-        # div = 1
-        # while x >= div * 10:
-        #     div *= 10
-
-        # while x:
-        #     left = x // div 
-        #     right = x % 10
-            
-        #     if right != left:
-        #         return False
-            
-        #     x = (x % div) // 10
-        #     div //= 100
-        # return True
-
-
-
-
-
-
-
-
-
-
-
-        # if x < 0: return False
-
-        # div = 1
-        # while x >= 10 * div:
-        #     div *= 10
-        
-        # while x:
-        #     right = x % 10
-        #     left = x // div
-
-        #     if right != left: return False
-
-        #     x = (x % div) // 10
-        #     div /= 100
-            
-        # return True
-            
-        
