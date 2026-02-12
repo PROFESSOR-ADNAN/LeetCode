88b19859a0d637c336564562557c81b0effd6d26
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-02-12 10:33:25 +0300

    Time: 3 ms (80.68%) | Memory: 19.2 MB (62.51%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,40 +1,28 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        romanToInt = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanToInteger = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        num = 0
-        # for i in range(len(s)):
-        #     if s[i] == "I":
-        #         if i+1 < len(s) and (s[i+1] == "V" or s[i+1] == "X"):
-        #             num -= romanToInt[s[i]]
-        #         else:
-        #             num += romanToInt[s[i]]
-        #     elif s[i] == "X":
-        #         if i+1 < len(s) and (s[i+1] == "L" or s[i+1] == "C"):
-        #             num -= romanToInt[s[i]]
-        #         else: 
-        #             num += romanToInt[s[i]]
-        #     elif s[i] == "C":
-        #         if i+1 < len(s) and (s[i+1] == "D" or s[i+1] == "M"):
-        #             num -= romanToInt[s[i]]
-        #         else:
-        #             num += romanToInt[s[i]]
-        #     else:
-        #         num += romanToInt[s[i]]
-
-        # return num
+        integer = 0
 
         for i in range(len(s)):
-            curr = romanToInt[s[i]]
-            nextVal = romanToInt[s[i+1]] if i+1 < len(s) else 0
-
-            if curr < nextVal:
-                num -= curr
+            roman_symbol = s[i]
+            if roman_symbol == "I":
+                if i+1 < len(s) and (s[i+1] == "V" or s[i+1] == "X"):
+                    integer -= romanToInteger[roman_symbol]
+                else:
+                    integer += romanToInteger[roman_symbol]
+            elif roman_symbol == "X":
+                if i+1 < len(s) and (s[i+1] == "L" or s[i+1] == "C"):
+                    integer -= romanToInteger[roman_symbol]
+                else:
+                    integer += romanToInteger[roman_symbol]
+            elif roman_symbol == "C":
+                if i+1 < len(s) and (s[i+1] == "D" or s[i+1] == "M"):
+                    integer -= romanToInteger[roman_symbol]
+                else:
+                    integer += romanToInteger[roman_symbol]
             else:
-                num += curr
+                integer += romanToInteger[roman_symbol]
 
-        return num
+        return integer
+            
