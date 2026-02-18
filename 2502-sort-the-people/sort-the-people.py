3d55e01a42e9c675af307340c6ca55249b1c758c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-02-18 15:35:06 +0300

    Time: 269 ms (13.47%) | Memory: 19.8 MB (16.95%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,3 +1,23 @@
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+#         def swap(i, j):
+#             heights[i], heights[j] = heights[j], heights[i]
+#             names[i], names[j] = names[j], names[i]
+
+#         n = len(heights)
+#         for i in range(n):
+#             swapped = False
+#             for j in range(n-i-1):
+#                 if heights[j] < heights[j+1]:
+#                     swap(j, j+1)
+#                     swapped = True
+            
+#             if not swapped:
+#                 break
+        
+#         return names
+                    
+
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         def swap(i, j):
@@ -6,14 +26,11 @@ class Solution:
 
         n = len(heights)
         for i in range(n):
-            swapped = False
-            for j in range(n-i-1):
-                if heights[j] < heights[j+1]:
-                    swap(j, j+1)
-                    swapped = True
+            min_ind = i
+            for j in range(i+1, n):
+                if heights[j] > heights[min_ind]:
+                    min_ind = j
+
+            swap(i, min_ind)
             
-            if not swapped:
-                break
-        
         return names
-                    
