22e445c4dfb5f7568e5a96d92c0070535668e22a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-02-21 22:01:29 +0300

    Time: 175 ms (13.41%) | Memory: 19.4 MB (48.09%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,67 +1,12 @@
-class Solution(object):
-    def smallerNumbersThanCurrent(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """ 
-        max_value = max(nums)
-        count = [0] * (max_value + 1)
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        ans = []
         for i in range(len(nums)):
-            count[nums[i]] += 1
-        for i in range(1, len(count)):
-            count[i] += count[i-1]
+            count = 0
+            for j in range(len(nums)):
+                if i != j and nums[j] < nums[i]:
+                    count += 1
+            ans.append(count)
         
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums[i] = 0
-            else:
-                nums[i] = count[nums[i]-1] 
-        return nums
+        return ans
 
-
-        # ind = []
-        # for i in range(len(nums)):
-        #     ind.append([nums[i], i])
-        # ind.sort()
-        
-        # nums[ind[0][1]] = 0
-        # for i in range(1, len(nums)):
-        #     if ind[i][0] != ind[i-1][0]:
-        #         nums[ind[i][1]] = i
-        #     else:
-        #         nums[ind[i][1]] = nums[ind[i-1][1]]
-        
-        # return nums
-
-
-        # ans = []
-        # for i in range(len(nums)):
-        #     count = 0
-        #     for j in range(len(nums)):
-        #         if i == j:
-        #             continue
-        #         if nums[i] > nums[j]:
-        #             count += 1
-        #     ans.append(count)
-        # return ans
-
-
-
-
-        # max_value = max(nums) 
-        # min_value = min(nums)
-        # range_value = max_value - min_value + 1
-
-        # count = [0] * range_value
-        # for num in nums:
-        #     count[num-min_value] += 1
-
-        # for i in range(1, len(count)):
-        #     count[i] += count[i-1]
-
-        # for i in range(len(nums)):
-        #     if nums[i] == min_value:
-        #         nums[i] = 0
-        #     else:
-        #         nums[i] = count[nums[i] - min_value - 1]
-        # return nums
