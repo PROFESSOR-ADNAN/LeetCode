0902f76ffc7b2a759d8340ef9c427a4c4a0b6fc0
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-02-24 14:48:03 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.32%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,58 +1,6 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        l, r = 0, len(needle)
-        while r <= len(haystack):
-            if haystack[l:r] == needle:
-                return l
-            l += 1
-            r += 1
-
-        return -1
-
-
-
-
-
-
-        # n = len(needle)
-        # for i in range(len(haystack)-n+1):
-        #     temp = ''
-        #     for j in range(i, i+n):
-        #         temp += haystack[j]
-        #     if temp == needle:
-        #         return i
-        # return -1
-
-
-
-
-
-
-
-
-
-
-
-        # l, r = 0, 0
-        # s = []
-        # while r < len(haystack):
-        #     s.append(haystack[r])
-        #     while len(s) > len(needle):
-        #         s.remove(haystack[l])
-        #         l += 1
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
             
-        #     if needle == ''.join(s):
-        #         return l
-        #     r += 1
-
-        # return -1
-
-
-
-
-        
+        return -1
