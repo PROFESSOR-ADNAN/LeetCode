b24bc9d722f41b1d15e8ae1b5cc62147f87612d6
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-02 15:32:00 +0300

    Time: 14 ms (32.95%) | Memory: 20.6 MB (29.29%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -1,49 +1,71 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        def index_inbound(r, c):
-            return 0 <= r < rows and 0 <= c < cols
+        # def index_inbound(r, c):
+        #     return 0 <= r < rows and 0 <= c < cols
 
+        # curr_row = rStart
+        # curr_col = cStart
+        # ans = []
+        # direction = "right"
+        # step = 2
+        # ans.append([curr_row, curr_col])
+
+        # while len(ans) < rows*cols:
+        #     if direction == "right":
+        #         for i in range(1, step):
+        #             curr_col += 1
+        #             if index_inbound(curr_row, curr_col):
+        #                 ans.append([curr_row, curr_col])
+                        
+        #         direction = "down"
+        #     elif direction == "down":
+        #         for i in range(1, step):
+        #             curr_row += 1
+        #             if index_inbound(curr_row, curr_col):
+        #                 ans.append([curr_row, curr_col])
+                    
+        #         step += 1
+        #         direction = "left"
+
+        #     elif direction == "left":
+        #         for i in range(1, step):
+        #             curr_col -= 1
+        #             if index_inbound(curr_row, curr_col):
+        #                 ans.append([curr_row, curr_col])
+        
+        #         direction = "up"
+
+        #     else:
+        #         for i in range(1, step):
+        #             curr_row -= 1
+        #             if index_inbound(curr_row, curr_col):
+        #                 ans.append([curr_row, curr_col])
+        
+        #         step += 1
+        #         direction = "right"
+
+        # return ans
+
+
+        direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+        ans = []
+        step = 1
+        ind = 0
         curr_row = rStart
         curr_col = cStart
-        ans = []
-        direction = "right"
-        step = 2
-        ans.append([curr_row, curr_col])
 
-        while len(ans) < rows*cols:
-            if direction == "right":
-                for i in range(1, step):
-                    curr_col += 1
-                    if index_inbound(curr_row, curr_col):
+        while len(ans) < rows * cols:
+            for _ in range(2):
+                for _ in range(step):
+                    if (0 <= curr_row < rows and 0 <= curr_col < cols):
                         ans.append([curr_row, curr_col])
-                        
-                direction = "down"
-            elif direction == "down":
-                for i in range(1, step):
-                    curr_row += 1
-                    if index_inbound(curr_row, curr_col):
-                        ans.append([curr_row, curr_col])
+                    r, c = direction[ind]
+                    curr_row += r
+                    curr_col += c
+
+                ind = (ind + 1) % 4
+
+            step += 1
                     
-                step += 1
-                direction = "left"
-
-            elif direction == "left":
-                for i in range(1, step):
-                    curr_col -= 1
-                    if index_inbound(curr_row, curr_col):
-                        ans.append([curr_row, curr_col])
-        
-                direction = "up"
-
-            else:
-                for i in range(1, step):
-                    curr_row -= 1
-                    if index_inbound(curr_row, curr_col):
-                        ans.append([curr_row, curr_col])
-        
-                step += 1
-                direction = "right"
-
         return ans
-
-
