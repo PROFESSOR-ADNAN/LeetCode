d8f9d09d73d202b9a5d9cc97787256e726c6737c
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-03 15:29:03 +0300

    Time: 7 ms (57.60%) | Memory: 21.1 MB (78.86%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = 0
-        candidate = 0
-        for num in nums:
-            if count == 0:
-                candidate = num
-
-            if num == candidate:
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maj = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] == maj:
                 count += 1
             else:
                 count -= 1
 
-        return candidate
+            if count == 0:
+                maj = nums[i]
+                count += 1
+
+        return maj
