18a734811d16248d94d22375aa52b44b37c4ee51
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-03 17:22:29 +0300

    Time: 7 ms (67.29%) | Memory: 22 MB (88.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -9,31 +9,92 @@ class Solution:
 
         # return ans
 
+        # cnt = len(nums)//3
 
+        # count = Counter(nums)
 
-
-
-
-
-
-
-
-
-
-
-
-
-        cnt = len(nums)//3
-
-        count = Counter(nums)
-
-        ans = []
-        for key, value in count.items():
-            if value > cnt:
-                ans.append(key)
+        # ans = []
+        # for key, value in count.items():
+        #     if value > cnt:
+        #         ans.append(key)
             
-        return ans
+        # return ans
+        # n = len(nums)-1
 
+        # c1, cnt1 = nums[0], 1
+        # c2, cnt2 = nums[n-1], 1
+
+        # for i in range(1,n-1):
+        #     curr_front = nums[i]
+        #     curr_back = nums[n-i]
+        #     if curr_front == c1:
+        #         cnt1 += 1
+        #     else:
+        #         cnt1 -= 1
+
+        #     if curr_back == c2:
+        #         cnt2 += 1
+        #     else:
+        #         cnt2 -= 1
+            
+        #     if cnt1 == 0:
+        #         c1 = curr
+
+        #     if cnt2 == 0:
+        #         c2 = cur
+
+        # print(c1, c2)
+        # return []
+
+
+
+        # n = len(nums)
+        # ans = []
+
+        # if cnt1 > n//3:
+        #     ans.append(cand1)
+        
+        # if cnt2 > n//3:
+        #     ans.append(cand2)
+
+        # return ans
+
+
+        cand1, cand2 = 0, 0
+        cnt1, cnt2 = 0, 0
+
+        for num in nums:
+            if (cnt1 and cnt2) or cand1 == num or cand2 == num:
+                if num == cand1:
+                    cnt1 += 1
+                elif num == cand2:
+                    cnt2 += 1
+                else:
+                    cnt1 -= 1
+                    cnt2 -= 1
+            else:
+                if cnt1 == 0:
+                    cand1 = num
+                    cnt1 += 1
+                else:
+                    cand2 = num
+                    cnt2 += 1
+
+        cnt1, cnt2 = 0, 0
+        for num in nums:
+            if num == cand1:
+                cnt1 += 1
+            elif num == cand2:
+                cnt2 += 1
+        
+        n = len(nums)
+        ans = []
+        if cnt1 > n//3:
+            ans.append(cand1)
+        if cnt2 > n//3:
+            ans.append(cand2)
+
+        return ans
 
 
 
