4fb61e21fa8056f5beeedb69ba0a13d0bc9b02b6
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-05 17:07:53 +0300

    Time: 25 ms (47.53%) | Memory: 27.6 MB (23.94%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,35 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        output = []
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # ans = []
+        # for i in range(len(nums)):
+        #     product = 1
+        #     for j in range(len(nums)):
+        #         if i != j:
+        #             product *= nums[j]
+                
+        #     ans.append(product)
 
-        prefix = 1
-        for num in nums:
-            output.append(prefix)
-            prefix *= num
+        # return ans
 
-        prefix = 1
-        for i in range(len(nums)-1, -1, -1):
-            output[i] *= prefix
-            prefix *= nums[i]
+        # [1, 2, 3, 4]
 
-        return output
+        # [1, 1, 2,  6, 24] # prefix[i]
+        #   [24, 24, 12, 4, 1] # suffix[i+1]
 
-        
+        # [24, 12, 8, 6]
 
-        
+        n = len(nums)
+
+        prefix = [1]
+        for i in range(n):
+            prefix.append(prefix[i] * nums[i])
+
+        suffix = [1] * (n+1)
+        for i in range(n-1, -1, -1):
+            suffix[i] = (suffix[i+1] * nums[i])
+
+        ans = []
+        for i in range(n):
+            ans.append(prefix[i]*suffix[i+1])
+
+        return ans
