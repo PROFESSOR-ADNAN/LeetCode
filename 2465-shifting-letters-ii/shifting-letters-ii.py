4d65c2a1417b69fac7f6806f1285ad87362ef501
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-06 10:06:34 +0300

    Time: 59 ms (60.25%) | Memory: 42.8 MB (75.61%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,25 +1,60 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        ans = [0] * len(s)
-        for start, end, shift in shifts:
-            if shift == 1:
-                ans[start] += 1
-                if end + 1 < len(s):
-                    ans[end+1] -= 1
+        # ans = [0] * len(s)
+        # for start, end, shift in shifts:
+        #     if shift == 1:
+        #         ans[start] += 1
+        #         if end + 1 < len(s):
+        #             ans[end+1] -= 1
+        #     else:
+        #         ans[start] -= 1
+        #         if end + 1 < len(s):
+        #             ans[end+1] += 1
+
+        # for i in range(1, len(ans)):
+        #     ans[i] += ans[i-1]
+
+        # for i in range(len(s)):
+        #     ch = s[i]
+        #     ans[i] = (ans[i] + (ord(ch)-ord("a"))) % 26
+
+        # res = ""
+        # for val in ans:
+        #     res += chr(val + ord("a"))
+
+        # return res
+
+        Row = len(shifts)
+        Col = len(shifts[0])
+
+        n = len(s)
+        all_shifts = [0] * n
+
+        for l, r, d in shifts:
+            if d:
+                all_shifts[l] += 1
+                if r+1 < n:
+                    all_shifts[r+1] += -1
             else:
-                ans[start] -= 1
-                if end + 1 < len(s):
-                    ans[end+1] += 1
+                all_shifts[l] += -1
+                if r+1 < n:
+                    all_shifts[r+1] += 1
 
-        for i in range(1, len(ans)):
-            ans[i] += ans[i-1]
+        for i in range(1, n):
+            all_shifts[i] += all_shifts[i-1]
 
-        for i in range(len(s)):
+        ans = []
+        for i in range(n):
             ch = s[i]
-            ans[i] = (ans[i] + (ord(ch)-ord("a"))) % 26
+            shift = all_shifts[i]
 
-        res = ""
-        for val in ans:
-            res += chr(val + ord("a"))
+            new_ch = chr((((ord(ch)-ord("a"))+shift) % 26) + ord("a"))
+            ans.append(new_ch)
 
-        return res
+        return "".join(ans)
+
+
+
+
+
+
