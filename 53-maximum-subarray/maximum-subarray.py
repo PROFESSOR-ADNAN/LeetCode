2b32d4eeab3b60f4387bf4decf5f13dbe34524cd
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-06 09:47:12 +0300

    Time: 28 ms (79.38%) | Memory: 31.4 MB (66.34%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,14 +1,49 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        curr_sum = nums[0]
-        max_sum = curr_sum
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # for i in range(1, len(nums)):
+        #     nums[i] += nums[i-1]
 
-        for i in range(1, len(nums)):
-            curr_sum = max(nums[i], nums[i]+curr_sum)
-            max_sum = max(max_sum, curr_sum)
+        # return max(nums)
+
+        # sm = sum(nums)
+        # mx_first = max(nums)
+        # temp = nums.copy()
+
+        # for i in range(1, len(temp)):
+        #     temp[i] += temp[i-1]
+
+        # mx = max(temp)
+        # mn = min(0, min(temp))
+
+        # if sm < 0:
+        #     return mx_first
+        
+        # return mx-mn
+
+        # max_sum = float("-inf")
+        # for i in range(len(nums)):
+        #     curr = 0
+        #     for j in range(i, len(nums)):
+        #         curr += nums[j]
+
+        #         max_sum = max(max_sum, curr)
+
+        # return max_sum
+
+        max_sum = float("-inf")
+        curr = 0
+
+        for i in range(len(nums)):
+            curr += nums[i]
+
+            max_sum = max(max_sum, curr)
+
+            if curr < 0:
+                curr = 0
 
         return max_sum
+
+
+            
+
+        
