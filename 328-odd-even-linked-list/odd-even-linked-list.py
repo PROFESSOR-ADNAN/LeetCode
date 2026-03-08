afa0b1c2e511f94d98d3418530c1cc1948fc90d9
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-08 15:27:49 +0300

    Time: 2 ms (19.93%) | Memory: 21.3 MB (36.55%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # dummy = ListNode()
+
+        # list1 = head
+        # list2 = head.next
+        # list3 = dummy
+
+        # while list1.next:
+        #     list3.next = list1
+        #     list3 = list3.next
+
+        #     list1 = list1.next.next
+
+        # if list1: 
+        #     list3.next = list1
+        #     list3 = list3.next
+
+        # while list2:
+        #     list3 = list2
+        #     if list3.next: list3 = list3.next
+
+        #     if list2.next: list2 = list2.next.next
+        #     else: break
+
+        # # if list2:
+        # #     list3.next = list2
+
+        # return dummy.next
+
+        if not head: return None
+
+        dummy = ListNode()
+
+        list1 = head
+        list2 = head.next
+        list3 = dummy
+
+        while list1 and list1.next:
+            list3.next = ListNode(list1.val)
+            list3 = list3.next
+
+            list1 = list1.next.next
+        
+        if list1:
+            list3.next = ListNode(list1.val)
+            list3 = list3.next
+
+        while list2 and list2.next:
+            list3.next = ListNode(list2.val)
+            list3 = list3.next
+
+            list2 = list2.next.next
+
+        if list2:
+            list3.next = ListNode(list2.val)
+            list3 = list3.next
+
+        return dummy.next
