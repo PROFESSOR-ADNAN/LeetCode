0ed002c9d7a9d0362bb21b4665d3936abc6670e5
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-10 16:55:50 +0300

    Time: 224 ms (32.75%) | Memory: 29.8 MB (95.05%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,44 +1,54 @@
-class Solution(object):
-    def longestSubarray(self, nums, limit):
-        """
-        :type nums: List[int]
-        :type limit: int
-        :rtype: int
-        """
-        maxQ = deque()
-        minQ = deque()
-        start = 0
-        max_len = 0
-        for end, num in enumerate(nums):
-            while maxQ and maxQ[-1] < num:
-                maxQ.pop()
-            while minQ and minQ[-1] > num:
-                minQ.pop()
-
-            maxQ.append(num)
-            minQ.append(num)
-
-            while maxQ[0] - minQ[0] > limit:
-                val = nums[start]
-                if maxQ[0] == val:
-                    maxQ.popleft()
-                if minQ[0] == val:
-                    minQ.popleft()
-                start += 1
-            max_len = max(max_len, end-start+1)
-            
-        return max_len
-       
-        # q = deque()
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        # n = len(nums)
         # max_len = 0
-        # for num in nums:
-        #     while q and q[0] - num > limit:
-        #         q.popleft()
+        # for i in range(n):
+        #     min_, max_ = float("inf"), 0
+        #     for j in range(i, n):
+        #         max_ = max(max_, nums[j])
+        #         min_ = min(min_, nums[j])
 
-        #     while q and q[-1] < num:
-        #         max_len = max(max_len, len(q)+1)
-        #         q.pop()
-            
-        #     q.append(num)
+        #         if max_ - min_ <= limit:
+        #             max_len = max(max_len, j-i+1)
 
         # return max_len
+
+        n = len(nums)
+        max_len = 0
+        minStack = deque()
+        maxStack = deque()
+
+        left = 0
+        for right in range(n):
+            
+            num = nums[right]
+
+            while maxStack and maxStack[0] < num:
+                maxStack.popleft()
+            while maxStack and maxStack[-1] < num:
+                maxStack.pop()
+            maxStack.append(num)
+
+            while minStack and minStack[0] > num:
+                minStack.popleft()
+            while minStack and minStack[-1] > num:
+                minStack.pop()
+            minStack.append(num)
+
+            while maxStack and minStack and maxStack[0] - minStack[0] > limit:
+                # minStack.pop(nums[left])
+                # maxStack.pop(nums[left])
+                element = nums[left]
+                if maxStack[0] == element:
+                    maxStack.popleft()
+                if minStack[0] == element:
+                    minStack.popleft()
+                left += 1
+
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+            
+
+
+
