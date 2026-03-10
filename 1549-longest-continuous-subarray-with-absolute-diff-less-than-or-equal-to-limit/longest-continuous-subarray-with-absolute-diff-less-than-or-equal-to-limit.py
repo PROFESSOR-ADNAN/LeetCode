dcd0f42321cc3142c964b9000a3abe45b911102b
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-10 16:58:06 +0300

    Time: 198 ms (48.30%) | Memory: 30.1 MB (83.48%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -13,6 +13,44 @@ class Solution:
 
         # return max_len
 
+        # n = len(nums)
+        # max_len = 0
+        # minStack = deque()
+        # maxStack = deque()
+
+        # left = 0
+        # for right in range(n):
+            
+        #     num = nums[right]
+
+        #     while maxStack and maxStack[0] < num:
+        #         maxStack.popleft()
+        #     while maxStack and maxStack[-1] < num:
+        #         maxStack.pop()
+        #     maxStack.append(num)
+
+        #     while minStack and minStack[0] > num:
+        #         minStack.popleft()
+        #     while minStack and minStack[-1] > num:
+        #         minStack.pop()
+        #     minStack.append(num)
+
+        #     while maxStack and minStack and maxStack[0] - minStack[0] > limit:
+        #         # minStack.pop(nums[left])
+        #         # maxStack.pop(nums[left])
+        #         element = nums[left]
+        #         if maxStack[0] == element:
+        #             maxStack.popleft()
+        #         if minStack[0] == element:
+        #             minStack.popleft()
+        #         left += 1
+
+        #     max_len = max(max_len, right - left + 1)
+
+        # return max_len
+            
+
+
         n = len(nums)
         max_len = 0
         minStack = deque()
@@ -23,21 +61,15 @@ class Solution:
             
             num = nums[right]
 
-            while maxStack and maxStack[0] < num:
-                maxStack.popleft()
             while maxStack and maxStack[-1] < num:
                 maxStack.pop()
             maxStack.append(num)
-
-            while minStack and minStack[0] > num:
-                minStack.popleft()
+            
             while minStack and minStack[-1] > num:
                 minStack.pop()
             minStack.append(num)
 
             while maxStack and minStack and maxStack[0] - minStack[0] > limit:
-                # minStack.pop(nums[left])
-                # maxStack.pop(nums[left])
                 element = nums[left]
                 if maxStack[0] == element:
                     maxStack.popleft()
@@ -49,6 +81,7 @@ class Solution:
 
         return max_len
             
+
 
 
 
