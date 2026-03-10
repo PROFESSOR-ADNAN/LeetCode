d705d7cef86ad7d57442ae2bec5d76da217d9b8a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-10 14:46:14 +0300

    Time: 113 ms (5.13%) | Memory: 19.3 MB (89.92%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,33 +1,14 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        stack = []
-        nextGreaterElement = {}
-        for num in nums2[::-1]:
-            while stack and stack[-1] <= num:
-                stack.pop()
-            nextGreaterElement[num] = -1 if not stack else stack[-1]
-            stack.append(num)  
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+        for num in nums1:
+            curr = -1
+            for j in range(len(nums2)):
+                if num == nums2[j]:
+                    for k in range(j+1, len(nums2)):
+                        if  nums2[k] > num:
+                            curr = nums2[k]
+                            break
+            ans.append(curr)
 
-        return [nextGreaterElement[num] for num in nums1] 
-
-        
-        
-        # ans = []
-        # for i in range(len(nums1)):
-        #     j = 0
-        #     while nums2[j] != nums1[i]:
-        #         j += 1
-            
-        #     for k in range(j+1, len(nums2)):
-        #         if nums2[k] > nums1[i]:
-        #             ans.append(nums2[k])
-        #             break
-        #     else:
-        #         ans.append(-1)
-
-        # return ans
+        return ans
