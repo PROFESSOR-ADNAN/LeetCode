4caaaf101b78cc944db06802a3bcf76add3559da
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-10 15:24:05 +0300

    Time: 144 ms (9.08%) | Memory: 36 MB (9.68%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,15 +1,31 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        stack = []
-        warmerTemp = defaultdict(lambda: 0)
-        for ind, temp in enumerate(temperatures):
-            while stack and stack[-1][0] < temp:
-                warmerTemp[stack[-1][1]] = ind - stack[-1][1]
-                stack.pop()
-            stack.append([temp, ind])
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # ans = []
+        # for i in range(len(temperatures)):
+        #     curr = 0
+        #     for j in range(i+1, len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             curr = j-i
+        #             break
+        #     ans.append(curr)
 
-        return [warmerTemp[i] for i in range(len(temperatures))]
+        # return ans
+
+        n = len(temperatures)
+        stack = []
+        ans = [0] * n
+
+        for i in range(n):
+            temp = temperatures[i]
+            while stack and stack[-1][0] < temp:
+                ind = stack[-1][1]
+                days = i - stack[-1][1]
+                ans[ind] = days
+                stack.pop()
+            
+            stack.append([temp, i])
+
+        return ans
+
+
+
