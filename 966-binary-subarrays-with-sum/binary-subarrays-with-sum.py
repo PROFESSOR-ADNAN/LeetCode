a6c80a74d9fd8a83bcdc11a6a9a1d0861e1098a7
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-11 15:14:17 +0300

    Time: 35 ms (53.23%) | Memory: 20.2 MB (72.58%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -36,23 +36,43 @@ class Solution:
 
         # return count
 
-        def helper(num, nums):
+        # def helper(num, nums):
+        #     n = len(nums)
+        #     left = 0
+        #     curr = 0
+        #     count = 0
+
+        #     for right in range(n):
+        #         curr += nums[right]
+                
+        #         while left <= right and curr > num:
+        #             curr -= nums[left]
+        #             left += 1
+
+        #         if curr <= num:
+        #             count += right - left + 1
+
+        #     return count
+
+        
+        # return helper(goal, nums) - helper(goal-1, nums)
+
+
+        def helper(nums, k):
             n = len(nums)
-            left = 0
             curr = 0
             count = 0
 
+            left = 0
             for right in range(n):
                 curr += nums[right]
-                
-                while left <= right and curr > num:
+
+                while left <= right and curr > k:
                     curr -= nums[left]
                     left += 1
-
-                if curr <= num:
-                    count += right - left + 1
+                
+                count += right - left
 
             return count
 
-        
-        return helper(goal, nums) - helper(goal-1, nums)
+        return helper(nums, goal) - helper(nums, goal-1)
