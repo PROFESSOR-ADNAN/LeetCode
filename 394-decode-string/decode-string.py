a500f18e9a94335197c651477ea38466cc624ff9
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-14 16:03:55 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.04%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,29 +1,45 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-    
-        def isLetterOrBracket(ch):
-            if len(ch) > 1: return True
-            return (ord("a") <= ord(ch) <= ord('z')) or (ch == "[") or (ch == "]")
+        # def isLetterOrBracket(ch):
+        #     if len(ch) > 1: return True
+        #     return (ord("a") <= ord(ch) <= ord('z')) or (ch == "[") or (ch == "]")
+
+        # stack = []
+        # for ch in s:
+        #     if ch != "]":
+        #         stack.append(ch)
+        #     else:
+        #         temp = []
+        #         while stack[-1] != "[":
+        #             top = stack.pop()
+        #             temp.append(top)
+        #         stack.pop()
+        #         numArr = []
+        #         while stack and not isLetterOrBracket(stack[-1]):
+        #             numArr.append(stack.pop())
+        #         numArr.reverse()
+        #         num = "".join(numArr)
+        #         num = int(num)
+        #         temp = num * temp[::-1]
+        #         stack.append("".join(temp))
+
+        # return "".join(stack)
 
         stack = []
+
         for ch in s:
             if ch != "]":
                 stack.append(ch)
             else:
-                temp = []
-                while stack[-1] != "[":
-                    top = stack.pop()
-                    temp.append(top)
+                subStr = ""
+                while stack and stack[-1] != "[":
+                    subStr = stack.pop() + subStr
                 stack.pop()
-                numArr = []
-                while stack and  not isLetterOrBracket(stack[-1]):
-                    numArr.append(stack.pop())
-                numArr.reverse()
-                num = "".join(numArr)
-                num = int(num)
-                temp = num * temp[::-1]
-                stack.append("".join(temp))
+
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+
+                stack.append(int(k) * subStr)
 
         return "".join(stack)
-
-
