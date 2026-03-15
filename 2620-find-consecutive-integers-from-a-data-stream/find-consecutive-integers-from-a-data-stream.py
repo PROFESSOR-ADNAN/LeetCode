a2d76edeaf32db3042a8c359015294ae82cded91
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-15 16:46:38 +0300

    Time: 68 ms (51.85%) | Memory: 51.6 MB (5.43%) - LeetSync

--- 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
@@ -1,88 +1,19 @@
-class DataStream(object):
+class DataStream:
 
-    def __init__(self, value, k):
-        """
-        :type value: int
-        :type k: int
-        """
+    def __init__(self, value: int, k: int):
+        self.stream = []
         self.value = value
         self.k = k
-        self.stream = deque()
-        self.count = 0
 
-    def consec(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-        if len(self.stream) == self.k:
-            if self.stream[0] == self.value:
-                self.count -= 1
-            self.stream.popleft()
-        self.stream.append(num)
-
+    def consec(self, num: int) -> bool:
         if num == self.value:
-            self.count += 1
+            self.stream.append(num)
+        else:
+            self.stream.clear()
 
-        return self.count == self.k
+        if len(self.stream) >= self.k: return True
+        else: return False
 
 # Your DataStream object will be instantiated and called as such:
 # obj = DataStream(value, k)
 # param_1 = obj.consec(num)
-
-# class DataStream(object):
-
-#     def __init__(self, value, k):
-#         """
-#         :type value: int
-#         :type k: int
-#         """
-#         self.value = value
-#         self.k = k
-#         self.count = 0
-
-#     def consec(self, num):
-#         """
-#         :type num: int
-#         :rtype: bool
-#         """
-#         if num == self.value:
-#             self.count += 1
-#         else:
-#             self.count = 0
-        
-#         return self.count >= self.k
-
-# # Your DataStream object will be instantiated and called as such:
-# # obj = DataStream(value, k)
-# # param_1 = obj.consec(num)
-
-# class DataStream(object):
-
-#     def __init__(self, value, k):
-#         """
-#         :type value: int
-#         :type k: int
-#         """
-#         self.stream = deque()
-#         self.value = value
-#         self.k = k
-
-#     def consec(self, num):
-#         """
-#         :type num: int
-#         :rtype: bool
-#         """
-#         self.stream.append(num)
-#         if self.stream and len(self.stream) > self.k:
-#             self.stream.popleft()     
-#         for i in range(len(self.stream)):
-#             if self.stream[i] != self.value:
-#                 return False
-#         return True if len(self.stream) == self.k else False
-
-
-
-# # Your DataStream object will be instantiated and called as such:
-# # obj = DataStream(value, k)
-# # param_1 = obj.consec(num)
