389e576e545de59189029e4af8566516e1764321
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-21 18:52:35 +0300

    Time: 148 ms (31.55%) | Memory: 24.5 MB (60.62%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,18 +1,38 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # operation = 0
+        # for i in range(n-2):
+        #     if nums[i] == 0:
+        #         nums[i] = 1
+        #         operation += 1
+        #         for j in range(i+1, i+3):
+        #             if nums[j] == 0:
+        #                 nums[j] = 1
+        #             else:
+        #                 nums[j] = 0
+
+        # if sum(nums) == len(nums):
+        #     return operation
+        # else:
+        #     return -1
+
+
+        def flip(nums, i):
+            nums[i] = 0 if nums[i] else 1
+
         n = len(nums)
         operation = 0
         for i in range(n-2):
             if nums[i] == 0:
-                nums[i] = 1
-                operation += 1
-                for j in range(i+1, i+3):
-                    if nums[j] == 0:
-                        nums[j] = 1
-                    else:
-                        nums[j] = 0
+                flip(nums, i)
+                flip(nums, i+1)
+                flip(nums, i+2)
 
-        if sum(nums) == len(nums):
-            return operation
-        else:
+                operation += 1  
+        
+        print(nums)
+        if not nums[-1] or not nums[-2]:
             return -1
+
+        return operation
