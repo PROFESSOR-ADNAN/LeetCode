4ce3aca58abbf487a78677dacb498e4eb2ff4fb3
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-24 21:38:00 +0300

    Time: 111 ms (48.45%) | Memory: 61.1 MB (96.37%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,19 +1,35 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        nums = [i for i in range(1, n+1)]
-        path = []
-        ans = []
+        # nums = [i for i in range(1, n+1)]
+        # path = []
+        # ans = []
 
-        def backtrack(cand):
-            if len(path) == k:
-                ans.append(path[:])
+        # def backtrack(cand):
+        #     if len(path) == k:
+        #         ans.append(path[:])
+        #         return
+
+        #     for i in range(len(cand)):
+        #         path.append(cand[i])
+        #         backtrack(cand[i+1:])
+        #         path.pop()
+
+
+        # backtrack(nums)
+        # return ans
+
+        ans = []
+        comb = []
+
+        def backtrack(start):
+            if len(comb) == k:
+                ans.append(comb[:])
                 return
 
-            for i in range(len(cand)):
-                path.append(cand[i])
-                backtrack(cand[i+1:])
-                path.pop()
+            for num in range(start, n+1):
+                comb.append(num)
+                backtrack(num+1)
+                comb.pop()
 
-
-        backtrack(nums)
+        backtrack(1)
         return ans
