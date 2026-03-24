6ffc24d557b82d9f20aeaa8d1bc0be5557cdf60e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-24 20:53:29 +0300

    Time: 48 ms (17.23%) | Memory: 20.6 MB (17.99%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -22,19 +22,41 @@ class Solution:
 
         # return isSame(root.left, subRoot) or isSame(root.right, subRoot)
 
-        def sameTree(root, subRoot):
-            if not root and not subRoot:
+        # def sameTree(root, subRoot):
+        #     if not root and not subRoot:
+        #         return True
+        #     # if root and subRoot and root.val == subRoot.val:
+        #     #     return (sameTree(root.left, subRoot.left) and sameTree(root.right, subRoot.right))
+        #     # return False
+
+        #     if not root or not subRoot or root.val != subRoot.val:
+        #         return False
+        #     return (sameTree(root.left, subRoot.left) and sameTree(root.right, subRoot.right))
+
+        # if not subRoot: return True
+        # if not root: return False
+
+        # if sameTree(root, subRoot):
+        #     return True
+
+        # return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+
+        def sameTree(a, b):
+            if not a and not b:
                 return True
-            # if root and subRoot and root.val == subRoot.val:
-            #     return (sameTree(root.left, subRoot.left) and sameTree(root.right, subRoot.right))
-            # return False
-
-            if not root or not subRoot or root.val != subRoot.val:
+            
+            if not a or not b or a.val != b.val:
                 return False
-            return (sameTree(root.left, subRoot.left) and sameTree(root.right, subRoot.right))
 
-        if not subRoot: return True
-        if not root: return False
+            return (sameTree(a.left, b.left) and sameTree(a.right, b.right))
+
+        
+        if not subRoot:
+            return True
+
+        if not root:
+            return False
 
         if sameTree(root, subRoot):
             return True
