277388afd3d636613eeaf696549233b37af10158
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-26 17:32:19 +0300

    Time: 5252 ms (11.16%) | Memory: 19.5 MB (27.48%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -0,0 +1,51 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        # cookies = [1, 2, 3]
+        # k = 4
+
+        # allSplit = []
+
+        # def backtrack(path):
+        #     if len(path) == len(cookies):
+        #         allSplit.append(path[:])
+        #         return
+
+        #     for j in range(len(cookies)):
+        #         if cookies[j] in path:
+        #             continue
+
+        #         curr = cookies[j]
+        #         path.append(curr)
+
+        #         backtrack(path)
+                 
+        #         path.pop()
+
+        # backtrack([])
+        # print(allSplit)
+        # return 0
+
+        min_unfairness = float("inf")
+        dist = [0] * k
+
+        def backtrack(i):
+            nonlocal min_unfairness, dist
+            if i == len(cookies):
+                min_unfairness = min(min_unfairness, max(dist))
+                return 
+
+            if min_unfairness <= max(dist): return
+
+            for j in range(k):
+                dist[j] += cookies[i]
+                backtrack(i+1)
+
+                dist[j] -= cookies[i]
+
+        backtrack(0)
+        return min_unfairness
+
+
+
+
+
