5beedaac45c5af7c3bf94c88a9adf21ee34218bc
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-26 11:22:11 +0300

    Time: 285 ms (5.01%) | Memory: 51.7 MB (5.97%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -15,20 +15,57 @@ class Solution:
         # return maxArea
 
 
-        stack = []
+        # stack = []
+        # maxArea = 0
+
+        # for ind, height in enumerate(heights):
+        #     start = ind
+        #     while stack and stack[-1][0] > height:
+        #         currHeight, currIndex = stack.pop()
+        #         maxArea = max(maxArea, (ind - currIndex) * currHeight)
+        #         start = currIndex
+
+        #     stack.append([height, start])
+
+        # while stack:
+        #     currHeight, currIndex = stack.pop()
+        #     maxArea = max(maxArea, (len(heights) - currIndex) * currHeight)
+
+        # return maxArea
+
+        nse = {}
+        def nextSE():
+            stack = []
+            for i in range(len(heights)):
+                height = heights[i]
+                while stack and heights[stack[-1]] > height:
+                    nse[stack.pop()] = i
+
+                stack.append(i)
+
+            while stack:
+                nse[stack.pop()] = len(heights)
+
+        pse = {}
+        def prevSE():
+            stack = []
+            for i in range(len(heights)):
+                height = heights[i]
+                while stack and heights[stack[-1]] > height:
+                    stack.pop()
+
+                if not stack:
+                    pse[i] = -1
+                else:
+                    pse[i] = stack[-1]
+
+                stack.append(i)
+        
+        prevSE()
+        nextSE()
+
         maxArea = 0
-
-        for ind, height in enumerate(heights):
-            start = ind
-            while stack and stack[-1][0] > height:
-                currHeight, currIndex = stack.pop()
-                maxArea = max(maxArea, (ind - currIndex) * currHeight)
-                start = currIndex
-
-            stack.append([height, start])
-
-        while stack:
-            currHeight, currIndex = stack.pop()
-            maxArea = max(maxArea, (len(heights) - currIndex) * currHeight)
+        for i in range(len(heights)): 
+            maxArea = max(maxArea, (nse[i]-pse[i]-1)*heights[i])
 
         return maxArea
