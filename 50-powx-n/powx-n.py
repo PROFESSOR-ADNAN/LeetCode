55547093990e00ee01fdd2a29c461efe28b5b427
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-27 11:07:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (41.17%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,35 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        # return x**n
+        memoization = defaultdict(int)
+        def pow(x, n):
+            if n == 1:
+                return x
+            if n == 0:
+                return 1
+
+            half = n // 2
+            if half in memoization:
+                res = memoization[half]
+            else:
+                res = pow(x, half)
+            memoization[half] = res
+            if n % 2:
+                return x * res * res
+            else:
+                return res * res
+
+        condition = False
+        if n < 0:
+            condition = True
+            n *= -1
+
+        ans = pow(x, n)
+
+        if condition:
+            return 1 / ans
+        else:
+            return ans
+
+       
+
