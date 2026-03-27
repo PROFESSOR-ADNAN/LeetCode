54d31b6731ac5111dfd7ade75893970b02ac8dff
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-27 11:36:13 +0300

    Time: 0 ms (100.00%) | Memory: 19.7 MB (14.47%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,35 +1,46 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         # return x**n
-        memoization = defaultdict(int)
-        def pow(x, n):
-            if n == 1:
-                return x
-            if n == 0:
-                return 1
 
-            half = n // 2
-            if half in memoization:
-                res = memoization[half]
-            else:
-                res = pow(x, half)
-            memoization[half] = res
-            if n % 2:
-                return x * res * res
-            else:
-                return res * res
+        # memoization = defaultdict(int)
+        # def pow(x, n):
+        #     if n == 1:
+        #         return x
+        #     if n == 0:
+        #         return 1
 
-        condition = False
-        if n < 0:
-            condition = True
-            n *= -1
+        #     half = n // 2
+        #     if half in memoization:
+        #         res = memoization[half]
+        #     else:
+        #         res = pow(x, half)
+        #     memoization[half] = res
+        #     if n % 2:
+        #         return x * res * res
+        #     else:
+        #         return res * res
 
-        ans = pow(x, n)
+        # condition = False
+        # if n < 0:
+        #     condition = True
+        #     n *= -1
 
-        if condition:
-            return 1 / ans
-        else:
-            return ans
+        # ans = pow(x, n)
 
-       
+        # if condition:
+        #     return 1 / ans
+        # else: 
+        #     return ans
 
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 1: return x
+            if n == 0: return 1
+
+            res = helper(x*x, n//2)
+            return x*res if n%2 else res
+
+        ans = helper(x, abs(n))
+        return 1/ans if n < 0 else ans
+
+    
