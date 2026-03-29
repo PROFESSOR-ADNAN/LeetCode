022291053ef48018dc6a441c534157b0aecf3bdd
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-29 21:00:12 +0300

    Time: 4 ms (32.34%) | Memory: 19.4 MB (33.15%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -20,23 +20,62 @@ class Solution:
         # return True
 
 
-        def backtrack(path, i):
+        # def backtrack(path, i):
+        #     if i >= len(s):
+        #         for ind in range(1, len(path)):
+        #             if int(path[ind]) != int(path[ind-1]) - 1:
+        #                 return False
+        #         return len(path) >= 2
+
+
+        #     for j in range(i, len(s)):
+        #         val = s[i:j+1]
+        #         path.append(val)
+        #         if backtrack(path, j+1):
+        #             return True
+                
+        #         path.pop()
+
+        #     return False
+                
+        # return backtrack([], 0)
+        
+
+        path = []
+        def backtrack(i):
             if i >= len(s):
-                for ind in range(1, len(path)):
-                    if int(path[ind]) != int(path[ind-1]) - 1:
+                for k in range(1, len(path)):
+                    if int(path[k]) != int(path[k-1]) - 1:
                         return False
+
                 return len(path) >= 2
 
 
             for j in range(i, len(s)):
-                val = s[i:j+1]
-                path.append(val)
-                if backtrack(path, j+1):
+                curr = s[i:j+1]
+
+                if path and int(curr) != int(path[-1]) - 1:
+                    continue
+
+                path.append(curr)
+                if backtrack(j+1):
                     return True
-                
                 path.pop()
 
             return False
-                
-        return backtrack([], 0)
-        
+
+        return backtrack(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
