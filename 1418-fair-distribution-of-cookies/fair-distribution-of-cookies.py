8dabd6ab19daceed36f99219905ede2ef211bf8a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-29 21:08:36 +0300

    Time: 1271 ms (28.25%) | Memory: 19.4 MB (56.87%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -25,25 +25,74 @@ class Solution:
         # print(allSplit)
         # return 0
 
+        # min_unfairness = float("inf")
+        # dist = [0] * k
+
+        # def backtrack(i):
+        #     nonlocal min_unfairness, dist
+        #     if i == len(cookies):
+        #         min_unfairness = min(min_unfairness, max(dist))
+        #         return 
+
+        #     if min_unfairness <= max(dist): return
+
+        #     for j in range(k):
+        #         dist[j] += cookies[i]
+        #         backtrack(i+1)
+
+        #         dist[j] -= cookies[i]
+
+        # backtrack(0)
+        # return min_unfairness
+
+        # min_unfairness = float("inf")
+        # dist = [0] * k
+
+        # def backtrack(i, j):
+        #     nonlocal min_unfairness, dist
+        #     print(dist)
+        #     if i == len(cookies):
+        #         min_unfairness = min(min_unfairness, max(dist))
+        #         return 
+        #     if j == k:
+        #         return 
+
+        #     dist[j] += cookies[i]
+        #     backtrack(i+1, j) 
+        #     dist[j] -= cookies[i]
+
+        #     backtrack(i, j+1)
+            
+
+        # backtrack(0, 0)
+        # return min_unfairness
+
         min_unfairness = float("inf")
         dist = [0] * k
 
         def backtrack(i):
-            nonlocal min_unfairness, dist
-            if i == len(cookies):
+            nonlocal dist, min_unfairness
+            if i >= len(cookies):
                 min_unfairness = min(min_unfairness, max(dist))
-                return 
-
-            if min_unfairness <= max(dist): return
+                return
 
             for j in range(k):
-                dist[j] += cookies[i]
-                backtrack(i+1)
+                curr = cookies[i]
 
-                dist[j] -= cookies[i]
+                if min_unfairness <= curr: return
+
+                dist[j] += curr
+                backtrack(i+1)
+                dist[j] -= curr
 
         backtrack(0)
         return min_unfairness
+
+
+
+
+
+
 
 
 
