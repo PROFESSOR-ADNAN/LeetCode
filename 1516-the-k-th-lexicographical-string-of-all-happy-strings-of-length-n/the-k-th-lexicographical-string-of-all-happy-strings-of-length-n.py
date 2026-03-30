3b60d9c9e6064a7549e70d938e2a41ba3142ad12
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-30 22:12:37 +0300

    Time: 71 ms (6.06%) | Memory: 29.8 MB (7.15%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,28 +1,67 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        char = ["a", "b", "c"]
+        # char = ["a", "b", "c"]
+        # ans = []
+        # path = []
+
+        # def backtrack():
+        #     if len(path) == n:
+        #         ans.append(path[:])
+        #         return
+
+        #     for j in range(3):
+        #         if path and path[-1] == char[j]:
+        #             continue
+
+        #         path.append(char[j])
+        #         backtrack()
+        #         path.pop()
+
+        # backtrack()
+
+        # if k <= len(ans):
+        #     return "".join(ans[k-1])
+        # else:
+        #     return ""
+
+
+
+
+
+
+
+        letters = ["a", "b", "c"]
         ans = []
         path = []
 
         def backtrack():
             if len(path) == n:
-                ans.append(path[:])
+                ans.append("".join(path))
                 return
 
             for j in range(3):
-                if path and path[-1] == char[j]:
+                if path and path[-1] == letters[j]:
                     continue
 
-                path.append(char[j])
+                path.append(letters[j])
                 backtrack()
                 path.pop()
 
         backtrack()
-
-        if k <= len(ans):
-            return "".join(ans[k-1])
-        else:
+        if k > len(ans):
             return ""
+        else:
+            return ans[k-1]
+
+
+
+
+
+
+
+
+
+
 
 
 
