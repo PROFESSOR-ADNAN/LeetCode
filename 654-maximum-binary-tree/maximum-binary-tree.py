0980c7af11f4388c9a426130c43e64e315eaee22
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-30 20:46:15 +0300

    Time: 33 ms (41.39%) | Memory: 20.9 MB (8.92%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.py
+++ 654-maximum-binary-tree/maximum-binary-tree.py
@@ -33,16 +33,55 @@ class Solution:
 
         # return root
 
-        if not nums:
-            return
+        # if not nums:
+        #     return
 
-        mx = max(nums)
-        idx = nums.index(mx)
+        # mx = max(nums)
+        # idx = nums.index(mx)
 
-        node = TreeNode(mx)
+        # node = TreeNode(mx)
 
-        node.left = self.constructMaximumBinaryTree(nums[0:idx])
-        node.right = self.constructMaximumBinaryTree(nums[idx+1:])
+        # node.left = self.constructMaximumBinaryTree(nums[0:idx])
+        # node.right = self.constructMaximumBinaryTree(nums[idx+1:])
 
-        return node
+        # return node
 
+
+
+        # n = len(nums)
+
+        # def constructor(left, right):
+        #     if left > right:
+        #         return None
+
+        #     mxIdx = left
+        #     for i in range(left, right+1):
+        #         if nums[i] > nums[mxIdx]:
+        #             mxIdx = i
+
+        #     left_node = constructor(left, mxIdx-1)
+        #     right_node = constructor(mxIdx+1, right)
+
+        #     return TreeNode(nums[mxIdx], left_node, right_node)
+
+        # return constructor(0, n-1)
+
+        n = len(nums)
+
+        def constructor(left, right):
+            if left > right:
+                return None
+
+            mxIdx = left
+            for i in range(left, right+1):
+                if nums[i] > nums[mxIdx]:
+                    mxIdx = i
+            
+            node = TreeNode(nums[mxIdx])
+
+            node.left = constructor(left, mxIdx-1)
+            node.right = constructor(mxIdx+1, right)
+
+            return node
+
+        return constructor(0, n-1)
