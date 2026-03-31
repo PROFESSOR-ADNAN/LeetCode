a705decb41d95158f2ea7d059a7ea6aa7432d4c3
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-31 18:08:35 +0300

    Time: 0 ms (100.00%) | Memory: 20.6 MB (74.86%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,16 +1,48 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        ans = [-1, -1]
-        flag1 = True
-        for i in range(len(nums)):
-            if flag1 and nums[i] >= target:
-                ans[0] = i
-                flag1 = False
+        # ans = [-1, -1]
+        # flag1 = True
+        # for i in range(len(nums)):
+        #     if flag1 and nums[i] >= target:
+        #         ans[0] = i
+        #         flag1 = False
 
-            if nums[i] == target:
-                ans[1] = i
+        #     if nums[i] == target:
+        #         ans[1] = i
 
-        if ans[0] >= 0 and ans[1] >= 0:
-            return ans
-        else:
-            return [-1, -1]
+        # if ans[0] >= 0 and ans[1] >= 0:
+        #     return ans
+        # else:
+        #     return [-1, -1]
+
+        start = end = -1
+
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                start = mid
+                right = mid-1
+            elif nums[mid] > target:
+                right = mid-1
+            else:
+                left = mid+1
+
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                end = mid
+                left = mid+1
+            elif nums[mid] > target:
+                right = mid-1
+            else:
+                left = mid+1
+
+        if start > -1 and  end > -1:
+            return [start, end]
+
+        return [-1, -1]
