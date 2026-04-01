4f274e2a2febcde690319134068dd5f897bee88a
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-01 19:55:26 +0300

    Time: 49 ms (24.04%) | Memory: 19.2 MB (80.62%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,14 +3,25 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        left, right = 0, n
-        ans = 0
-        while left <= right:
-            mid = (left + right) // 2
-            if isBadVersion(mid):
-                ans = mid
-                right = mid-1
-            else:
-                left = mid + 1
+        # left, right = 0, n
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if isBadVersion(mid):
+        #         right = mid-1
+        #     else:
+        #         left = mid + 1
 
-        return ans
+        # return left
+
+
+
+        low, high = 1, n
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
