b412421b1b59ecce31fd5a050048d4ed77c3fb0f
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-03 19:02:57 +0300

    Time: 0 ms (100.00%) | Memory: 24.6 MB (80.73%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        # def isValid(index):
+        #     curr_citation = citations[index]
+        #     return curr_citation >= index+1
+
+        # low, high = 0, len(citations)-1
+        # while low <= high:
+        #     mid = low + (high - low) // 2
+        #     if isValid(mid):
+        #         high = mid - 1
+        #     else:
+        #         low = mid + 1
+
+        # return citations[low]
+
+
+        # def isValid(h):
+        #     print(h, citations[h-1])
+        #     return citations[h-1] >= h
+
+
+        # low, high = 1, len(citations)
+        # while low <= high:
+        #     mid = low + (high - low) // 2
+        #     if isValid(mid):
+        #         low = mid
+        #     else:
+        #         high = mid - 1
+
+        # return high
+
+        citations.reverse()
+
+        def isValid(h):
+            print(h, citations[h-1])
+            return citations[h-1] >= h
+
+
+        low, high = 1, len(citations)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if isValid(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return high
