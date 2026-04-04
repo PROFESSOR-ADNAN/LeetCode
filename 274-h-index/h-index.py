4e5a5b34349d383ef89a22b857dd28191ff26751
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-04 07:23:38 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.96%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,9 +1,24 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        n = len(citations)
-        for i in range(n):
-            if citations[i] >= n - i:
-                return n - i
+        # citations.sort()
+        # n = len(citations)
+        # for i in range(n):
+        #     if citations[i] >= n - i:
+        #         return n - i
 
-        return 0
+        # return 0
+
+        citations.sort(reverse = True)
+
+        def isValid(ind):
+            return citations[ind-1] >= ind
+
+        low, high = 1, len(citations)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if isValid(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return high
