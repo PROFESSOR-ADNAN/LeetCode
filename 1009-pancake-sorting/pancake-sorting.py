2d899d4b4df32f206f529a994294ed2a0672d5ee
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-13 20:12:44 +0300

    Time: 5 ms (17.61%) | Memory: 19.3 MB (78.89%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -15,24 +15,47 @@ class Solution:
         # return ans
 
         
-        def flip(l, r):
-            while l < r:
-                arr[l], arr[r] = arr[r], arr[l]
-                l += 1
-                r -= 1
+        # def flip(l, r):
+        #     while l < r:
+        #         arr[l], arr[r] = arr[r], arr[l]
+        #         l += 1
+        #         r -= 1
 
-        n = len(arr)
+        # n = len(arr)
+        # ans = []
+
+        # for i in range(n):
+        #     mx = max(arr[:n-i])
+        #     ind = arr.index(mx)
+
+        #     flip(0, ind)
+        #     flip(0, n-i-1)
+
+        #     ans.append(ind+1)
+        #     ans.append(n-i)
+
+        # return ans
+
+        def flip(end):
+            start = 0
+            while start < end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+
+        N = len(arr)
         ans = []
 
-        for i in range(n):
-            mx = max(arr[:n-i])
-            ind = arr.index(mx)
+        for i in range(N-1, -1, -1):
+            mx = i
+            for j in range(i-1, -1, -1):
+                if arr[j] > arr[mx]: mx = j
 
-            flip(0, ind)
-            flip(0, n-i-1)
-
-            ans.append(ind+1)
-            ans.append(n-i)
+            if mx != i:
+                flip(mx)
+                flip(i)
+                ans.append(mx+1)
+                ans.append(i+1)
 
         return ans
 
