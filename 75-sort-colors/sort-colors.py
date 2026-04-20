7d6f7cca0998a1b0edcb06b92110e8e2865cb3d8
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-20 21:24:05 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.79%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -24,14 +24,48 @@ class Solution:
         # for i in range(len(nums)):
         #     nums[i] = ans[i]
 
-        count = [0] * 3
-        for num in nums:
-            count[num] += 1
+        # count = [0] * 3
+        # for num in nums:
+        #     count[num] += 1
 
+        # ind = 0
+        # for num, cnt in enumerate(count):
+        #     for _ in range(cnt):
+        #         nums[ind] = num
+        #         ind += 1
+
+        # left, right = 0, len(nums)-1
+        # for i, num in enumerate(nums):
+        #     print(nums)
+        #     if num == 0:
+        #         nums[left], nums[i] = nums[i], nums[left]
+        #         left += 1
+        #     elif num == 2 and nums[right] == 0:
+        #         nums[right], nums[left] = nums[left], nums[right]
+        #         left += 1
+        #         right -= 1
+        #     elif num == 2:
+        #         nums[right], nums[i] = nums[i], nums[right]
+        #         right -= 1
+
+
+        #     print(nums)
+        #     print()
+
+        left, right = 0, len(nums)-1
         ind = 0
-        for num, cnt in enumerate(count):
-            for _ in range(cnt):
-                nums[ind] = num
-                ind += 1
 
-            
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        while ind <= right:
+            if nums[ind] == 0:
+                swap(ind, left)
+                left += 1
+
+            elif nums[ind] == 2:
+                swap(ind, right)
+                right -= 1
+                ind -= 1
+                
+            ind += 1
