03d189fe5f119d4f65a8fbcbd27bec85cd84f199
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-20 17:31:06 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.22%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,36 +1,37 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
+        # count = Counter(nums)
+        # total = 0
+        # for i in range(1, 3):
+        #     if i not in nums:
+        #         continue
+        #     total += count[i-1]
+        #     count[i] = total
+        # count[0] = 0
+
+        # print(count)
+        # ans = [-1] * len(nums)
+        # for ind, num in enumerate(nums):
+        #     j = count[num]
+        #     while j < len(ans) and ans[j] == num:
+        #         j += 1
+
+        #     # ans[j] = num
+
+        # for i in range(len(nums)):
+        #     nums[i] = ans[i]
+
         count = [0] * 3
         for num in nums:
             count[num] += 1
-        
-        i = 0
-        for color in range(3):
-            for _ in range(count[color]):
-                nums[i] = color
-                i += 1
 
-        return nums
+        ind = 0
+        for num, cnt in enumerate(count):
+            for _ in range(cnt):
+                nums[ind] = num
+                ind += 1
 
-
-        # count = [0] * 3
-        # for num in nums:
-        #     count[num] += 1
-    
-        # for i in range(1, len(count)):
-        #     count[i] += count[i-1]
-      
-        # i = len(nums)-1
-        # ans = [0] * len(nums)
-
-        # while i >= 0:
-        #     ans[count[nums[i]] - 1] = nums[i]
-        #     count[nums[i]] -= 1
-        #     i -= 1
-        # for i in range(len(nums)):
-        #     nums[i] = ans[i]
-        # return nums
+            
