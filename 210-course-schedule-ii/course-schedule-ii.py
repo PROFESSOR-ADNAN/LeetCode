963a010fd03976d583148171905c459e0c9f1049
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-05-05 18:50:22 +0300

    Time: 1 ms (83.31%) | Memory: 20.3 MB (90.06%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,29 +1,50 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # indegree = [0] * numCourses
+        # graph = defaultdict(list)
+        # for u, v in prerequisites:
+        #     graph[v].append(u)
+        #     indegree[u] += 1
+
+        # white, grey, black = 0, 1, 2
+
+        # color = {course:white for course in range(numCourses)}
+
+        # def dfs(node):
+        #     color[node] = grey
+        #     for nei in graph[node]:
+        #         if color[nei] == grey: return True
+        #         if color[nei] == white:
+        #             if dfs(nei):
+        #                 return True
+
+        #     color[node] = black
+
+        # for i in range(numCourses):
+        #     if color[i] == white:
+        #         if dfs(i):
+        #             return []
+
+        # q = deque(i for i in range(numCourses) if indegree[i] == 0)
+
+        # top_order = []
+        # while q:
+        #     node = q.popleft()
+        #     top_order.append(node)
+
+        #     for nei in graph[node]:
+        #         indegree[nei] -= 1
+        #         if indegree[nei] == 0:
+        #             q.append(nei)
+
+        # return top_order
+
+
         indegree = [0] * numCourses
         graph = defaultdict(list)
         for u, v in prerequisites:
             graph[v].append(u)
             indegree[u] += 1
-
-        white, grey, black = 0, 1, 2
-
-        color = {course:white for course in range(numCourses)}
-
-        def dfs(node):
-            color[node] = grey
-            for nei in graph[node]:
-                if color[nei] == grey: return True
-                if color[nei] == white:
-                    if dfs(nei):
-                        return True
-                        
-            color[node] = black
-
-        for i in range(numCourses):
-            if color[i] == white:
-                if dfs(i):
-                    return []
 
         q = deque(i for i in range(numCourses) if indegree[i] == 0)
 
@@ -37,4 +58,4 @@ class Solution:
                 if indegree[nei] == 0:
                     q.append(nei)
 
-        return top_order
+        return top_order if len(top_order) == numCourses else []
