e120c0177c1b59ac9659553e97d9249f5cd37f29
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-05-08 17:25:23 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.76%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,17 +1,24 @@
-class Solution(object):
-    def lastStoneWeight(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
-        while len(stones) > 1:
-            last = heapq.heappop(stones)
-            second_to_last = heapq.heappop(stones)
-            if last != second_to_last:
-                diff = last - second_to_last
-                heapq.heappush(stones, diff)
-        stones.append(0)
-        return abs(stones[0])
-        
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # stones.sort()
+        # while len(stones) > 1:
+        #     a = stones.pop()
+        #     b = stones.pop()
+        #     if a != b:
+        #         stones.append(a-b)
+        #     stones.sort()
+
+        # return stones[0]
+
+        maxHeap = [-val for val in stones]
+        heapq.heapify(maxHeap)
+
+        while len(maxHeap) > 1:
+            a = -1 * heapq.heappop(maxHeap)
+            b = -1 * heapq.heappop(maxHeap)
+
+            if a != b:
+                maxHeap.append(-1 * (a-b))
+                heapq.heapify(maxHeap)
+
+        return -1 * heapq.heappop(maxHeap) if maxHeap else 0
