b0760f1d39ff07be429f2752d1b5f81d656ef248
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-05-13 20:09:47 +0300

    Time: 9 ms (64.26%) | Memory: 23 MB (48.86%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,48 +5,59 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
+    #     if not lists:
+    #         return None
 
-        while len(lists) > 1:
-            n = len(lists)
-            curr_merged_list = []
-            for i in range(0, n, 2):
-                if i+1 < n:
-                    curr_merged_list.append(self.merge_linked_list(lists[i], lists[i+1]))
-                else:
-                    curr_merged_list.append(lists[i])
+    #     while len(lists) > 1:
+    #         n = len(lists)
+    #         curr_merged_list = []
+    #         for i in range(0, n, 2):
+    #             if i+1 < n:
+    #                 curr_merged_list.append(self.merge_linked_list(lists[i], lists[i+1]))
+    #             else:
+    #                 curr_merged_list.append(lists[i])
 
-            lists = curr_merged_list
+    #         lists = curr_merged_list
 
-        return lists[0]
+    #     return lists[0]
 
-    def merge_linked_list(self, list1, list2):
+    # def merge_linked_list(self, list1, list2):
+    #     dummy = ListNode()
+    #     list3 = dummy
+
+    #     while list1 and list2:
+    #         if list1.val < list2.val:
+    #             list3.next = list1
+    #             list1 = list1.next
+    #         else:
+    #             list3.next = list2
+    #             list2 = list2.next
+
+    #         list3 = list3.next
+
+    #     if list1:
+    #         list3.next = list1
+    #         list3 = list3.next
+    #     if list2:
+    #         list3.next = list2
+    #         list3 = list3.next
+
+    #     return dummy.next
+
+
+        minHeap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heappush(minHeap, (lists[i].val, i, lists[i]))
+
         dummy = ListNode()
-        list3 = dummy
+        curr = dummy
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                list3.next = list1
-                list1 = list1.next
-            else:
-                list3.next = list2
-                list2 = list2.next
-
-            list3 = list3.next
-
-        if list1:
-            list3.next = list1
-            list3 = list3.next
-        if list2:
-            list3.next = list2
-            list3 = list3.next
+        while minHeap:
+            val, i, node = heappop(minHeap)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heappush(minHeap, (node.next.val, i, node.next))
 
         return dummy.next
-
-
-
-
-
-
-            
