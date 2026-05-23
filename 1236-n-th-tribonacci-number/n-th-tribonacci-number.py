cac3e954af70f0adb7d4371ea0f0e031427ffb02
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-05-23 21:25:21 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.87%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,29 +1,42 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
 
-        memo = [-1] *(n+1)
-        def helper(n):
-            nonlocal memo
+        # memo = [-1] *(n+1)
+        # def helper(n):
+        #     nonlocal memo
 
-            if n == 0: return 0
-            if n == 1 or n == 2: return 1
+        #     if n == 0: return 0
+        #     if n == 1 or n == 2: return 1
             
-            if memo[n-1] != -1:
-                a = memo[n-1]
-            else:
-                a = memo[n-1] = helper(n-1)
+        #     if memo[n-1] != -1:
+        #         a = memo[n-1]
+        #     else:
+        #         a = memo[n-1] = helper(n-1)
             
-            if memo[n-2] != -1:
-                b = memo[n-2]
-            else:
-                b = memo[n-2] = helper(n-2)
+        #     if memo[n-2] != -1:
+        #         b = memo[n-2]
+        #     else:
+        #         b = memo[n-2] = helper(n-2)
 
-            if memo[n-3] != -1:
-                c = memo[n-3]
-            else:
-                c = memo[n-3] = helper(n-3)
+        #     if memo[n-3] != -1:
+        #         c = memo[n-3]
+        #     else:
+        #         c = memo[n-3] = helper(n-3)
 
 
-            return a + b + c
+        #     return a + b + c
 
-        return helper(n)
+        # return helper(n)
+
+
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        tribonacci = [0, 1, 1]
+        for i in range(3, n+1):
+            sm = tribonacci[i-1] + tribonacci[i-2] + tribonacci[i-3]
+            tribonacci.append(sm)
+
+        return tribonacci[-1]
